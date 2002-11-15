1751c2b282d300d6207d77ef6e2834e45d2b461f
weilyn <weilyn@kestrel.edu>
2002-11-15 00:56:12 +0000

    Added trace and prof commands to standalone.
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@426 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- Applications/Specware/Handwritten/Lisp/BuildPreamble.lisp
+++ Applications/Specware/Handwritten/Lisp/BuildPreamble.lisp
@@ -11,6 +11,8 @@
 ;;; Normally autoloaded, but we want to preload them for a stand-alone world
 (require "eli")
 (require "sock")
+(require "trace")
+(require "prof")
 
 ;; Override normal definition because of an apparent Allegro bug in
 ;; generate-application where excl::compile-file-if-needed compiles
