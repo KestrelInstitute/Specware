b40af259ebd827b65979696a16523d7804c0ba1c
weilyn <weilyn@kestrel.edu>
2003-02-14 21:26:59 +0000

    Initial version.
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@767 7b97033b-253e-4a5c-9b07-e25f9089a9fd

---
+++ Gui/src/edu/kestrel/netbeans/actions/StartLispAction.java
@@ -0,0 +1,91 @@
+/*
+ * StartLispAction.java
+ *
+ * Created on February 14, 2003, 12:34 PM
+ */
+
+package edu.kestrel.netbeans.actions;
+
+import org.openide.filesystems.FileObject;
+import org.openide.loaders.DataObject;
+import org.openide.TopManager;
+import org.openide.util.HelpCtx;
+import org.openide.util.actions.NodeAction;
+import org.openide.nodes.Node;
+import org.openide.util.NbBundle;
+
+import edu.kestrel.netbeans.MetaSlangDataObject;
+import edu.kestrel.netbeans.Util;
+import edu.kestrel.netbeans.model.SourceElement;
+import edu.kestrel.netbeans.lisp.LispProcessManager;
+import org.openide.filesystems.FileStateInvalidException;
+
+/**
+ *
+ * @author  weilyn
+ */
+public class StartLispAction extends NodeAction {
+    /** generated Serialized Version UID */
+    static final long serialVersionUID = 5089785814030008826L;
+
+    /* execute code for the nodes */
+    protected void performAction (Node[] activatedNodes) {
+        startLisp();
+    }
+    
+    protected boolean enable(Node[] arr) {
+        if (arr.length == 0) {
+	    return false;
+	}
+	for (int i = 0; i < arr.length; i++) {
+            Node node = arr[i];
+	    if (!enable(node)) {
+		return false;
+	    }
+        }
+        return true;
+    }
+
+    private boolean enable (Node node) {
+	DataObject dataObj = (DataObject) node.getCookie(DataObject.class);
+	return (dataObj instanceof MetaSlangDataObject);	
+    }
+
+    /** Message to display when the action is looking for
+    * object that should be processed.
+    *
+    * @return text to display at status line
+    */
+    protected String message () {
+        return NbBundle.getMessage(StartLispAction.class, "CTL_ConnectingToLisp");
+    }
+
+    /** Execute code for a node.
+    * @param node the node
+    */
+    void startLisp () {
+        TopManager.getDefault ().setStatusText (message ());
+	LispProcessManager.destroyLispProcess();
+        if (LispProcessManager.connectToLisp()) {
+            TopManager.getDefault ().setStatusText (NbBundle.getMessage(StartLispAction.class, "CTL_NoConnectionToLisp"));
+        } else {
+        TopManager.getDefault ().setStatusText (NbBundle.getMessage(StartLispAction.class, "CTL_ConnectedToLisp"));
+        }
+    }
+    
+    /* Human presentable name of the action. This should be
+    * presented as an item in a menu.
+    * @return the name of the action
+    */
+    public String getName() {
+        return NbBundle.getMessage(StartLispAction.class, "StartLisp");
+    }
+
+    /* Help context where to find more about the action.
+    * @return the help context for this action
+    */
+    public HelpCtx getHelpCtx() {
+        return new HelpCtx(StartLispAction.class);
+    }
+    
+}
