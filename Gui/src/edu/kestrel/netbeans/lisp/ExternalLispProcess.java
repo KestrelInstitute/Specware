99afd8020363c2c36819b8487bb940334aa21ef3
weilyn <weilyn@kestrel.edu>
2003-02-14 21:19:42 +0000

    Added SPECWARE_ROOT variable, which should be changed to point to the Specware4 directory.  Changed the path to lisp files.
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@762 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- Gui/src/edu/kestrel/netbeans/lisp/ExternalLispProcess.java
+++ Gui/src/edu/kestrel/netbeans/lisp/ExternalLispProcess.java
@@ -21,16 +21,20 @@ import org.openide.util.Utilities;
  * @author  weilyn
  */
 public class ExternalLispProcess extends ProcessExecutor {
+    
+    // HACKY: change this to your root Specware directory for now...
+    private static final String SPECWARE_ROOT = "C:\\Progra~1\\Specware4\\";
+   
     private String lispImagePath;
     private NbProcessDescriptor DEFAULT;
     
     /** Creates a new instance of ExternalLispProcess */
     public ExternalLispProcess() {
 
-        lispImagePath = (Utilities.isWindows() ? "c:\\Specware4\\" : "~/specware/Specware4/") ;
+        lispImagePath = SPECWARE_ROOT;
         
-        String lispImageFile = (lispImagePath+ (Utilities.isWindows() ? "\\Applications\\Specware\\bin\\windows\\" : "Applications/Specware/bin/linux/") +"Specware.dxl"); 
-        String initFile = (lispImagePath + (Utilities.isWindows() ? "\\Gui\\src\\edu\\kestrel\\netbeans\\lisp\\" : "Gui/src/edu/kestrel/netbeans/lisp/") + "init-java-connection.lisp");
+        String lispImageFile = (lispImagePath+ (Utilities.isWindows() ? "\\Applications\\Specware\\bin\\windows\\" : "Applications/Specware/bin/linux/") +"Specware4.dxl"); 
+        String initFile = (lispImagePath + (Utilities.isWindows() ? "\\Gui\\src\\Lisp\\" : "Gui/src/Lisp/") + "specware-init.lisp");
         String lispExecutable = (Utilities.isWindows() ? "\"c:\\Progra~1\\acl62\\alisp.exe\"" : "/usr/local/acl/acl62/alisp");
         DEFAULT = new NbProcessDescriptor(lispExecutable, (("-I "+lispImageFile+" -L "+initFile)));
         setExternalExecutor(DEFAULT);
@@ -38,10 +42,6 @@ public class ExternalLispProcess extends ProcessExecutor {
     
     public Process createProcess() throws IOException {
         NbProcessDescriptor ee = getExternalExecutor();
-        System.out.println("Args are "+ee.getArguments()+", and Info is "+ee.getInfo());
- 
-        File testFile = new File("usr/local/acl/acl62/alisp");
-        System.out.println("alisp file is a file: "+testFile.isFile());
         
         return ee.exec();
     }
