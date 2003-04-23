720d97c6e1f6f8f1590d6e6e2d4b5ae3b0c1d86a
weilyn <weilyn@kestrel.edu>
2003-04-23 01:15:34 +0000

    DiagElemElementNode.java
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@967 7b97033b-253e-4a5c-9b07-e25f9089a9fd

---
+++ Gui/src/edu/kestrel/netbeans/nodes/UnitIDObjectNode.java
@@ -0,0 +1,21 @@
+/*
+ * UnitIDObjectNode.java
+ *
+ * Created on April 10, 2003, 11:12 AM
+ */
+
+package edu.kestrel.netbeans.nodes;
+
+
+/**
+ *
+ * @author  weilyn
+ */
+public class UnitIDObjectNode extends ObjectNode {
+    
+    /** Creates a new instance of UnitIDObjectNode */
+    public UnitIDObjectNode(Object object) {
+        super(object);
+    }
+    
+}
