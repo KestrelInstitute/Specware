7a6beec57063054548315165eca06a578eddf022
weilyn <weilyn@kestrel.edu>
2003-05-07 19:10:47 +0000

    support for gotoUnitDef
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@984 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- Gui/src/edu/kestrel/netbeans/actions/GoToUnitDefinitionAction.java
+++ Gui/src/edu/kestrel/netbeans/actions/GoToUnitDefinitionAction.java
@@ -7,8 +7,8 @@ import edu.kestrel.netbeans.Util;
 import edu.kestrel.netbeans.lisp.LispProcessManager;
 import edu.kestrel.netbeans.model.Element;
 import edu.kestrel.netbeans.model.MemberElement;
-import edu.kestrel.netbeans.model.SpecElement;
 import edu.kestrel.netbeans.model.SourceElement;
+import edu.kestrel.netbeans.model.SpecElement;
 import edu.kestrel.netbeans.nodes.MemberElementNode;
 
 import java.io.FileNotFoundException;
@@ -22,7 +22,6 @@ import org.openide.filesystems.FileStateInvalidException;
 import org.openide.filesystems.Repository;
 import org.openide.filesystems.FileObject;
 import org.openide.filesystems.FileSystem;
-import org.openide.filesystems.Repository;
 import org.openide.loaders.DataObject;
 import org.openide.loaders.DataObjectNotFoundException;
 import org.openide.nodes.Children;
@@ -100,6 +99,7 @@ public class GoToUnitDefinitionAction extends NodeAction {
             }
             MemberElementNode node = dereference(nodes[i].getName(), packageName, currFileName);
         }
+        TopManager.getDefault ().setStatusText (""); // NOI18N
     }
     
     /**
