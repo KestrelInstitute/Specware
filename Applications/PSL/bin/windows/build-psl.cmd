3b50bd94ae2c1cf0d8102338c9b81122ed0c834f
lindsay <lindsay@kestrel.edu>
2003-08-08 00:47:11 +0000

    Changed paths to XEmacs and Allegro
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@1201 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- Applications/PSL/bin/windows/build-psl.cmd
+++ Applications/PSL/bin/windows/build-psl.cmd
@@ -1,11 +1,11 @@
 set SPECWARE4=\Progra~1\Specware4
 rem SWPATH needs /s rather than \s so URI parsing works
 set SWPATH=/Progra~1/Specware4;/
-rem set XEMACS=C:\Progra~1\XEmacs\XEmacs-21.4.10
-set XEMACS=C:\Progra~1\XEmacs\XEmacs-21.4.6
+set XEMACS=C:\Progra~1\XEmacs\XEmacs-21.4.10
+rem set XEMACS=C:\Progra~1\XEmacs\XEmacs-21.4.6
 rem Set allegro to the version you have
-rem set ALLEGRO=C:\Progra~1\acl62
-set ALLEGRO=C:\Progra~1\acl61
+set ALLEGRO=C:\Progra~1\acl62
+rem set ALLEGRO=C:\Progra~1\acl61
 
 set SPECWARE_BIN=%SPECWARE4%\Applications\Specware\bin\windows
 
