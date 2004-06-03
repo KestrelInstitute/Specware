d56131156d5937f4c1e93a9f24513b2fd20a1436
Jim McDonald <mcdonald@kestrel.edu>
2004-06-03 02:07:42 +0000

    Add shell script for automatically running specware test suite.
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@2054 7b97033b-253e-4a5c-9b07-e25f9089a9fd

---
+++ Applications/Specware/Tests/test-specware4.lisp
@@ -0,0 +1,17 @@
+(in-package "USER")
+
+(defun test-specware4 (&optional (log-file-name
+				  (multiple-value-bind (ss mm hh dd mm yy)
+				      (decode-universal-time (get-universal-time))
+				    (format nil "/tmp/test-specware4-~D-~D-~D-~D-~D-~D"
+					    yy mm dd hh mm ss))))
+  (format t "~&Testing Specware4, results to ~A ...~%" log-file-name)
+  (with-open-file (s log-file-name :direction :output) 
+    (let ((*standard-output* s)
+	  (*error-output* s)
+	  (test-dir (format nil "~A/TestSuite/" (specware::getenv "SPECWARE4"))))
+      (specware-test::run-test-directories-rec-fn (list test-dir))))
+  (format t "~&Done.~%")
+  log-file-name)
+
+
