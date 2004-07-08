a1d140644157097b98d9ff5348193ed2419ce268
Alessandro Coglio <coglio@kestrel.edu>
2004-07-08 18:36:58 +0000

    changed Windows scripts to build/run/etc. to set SWPATH only if not already (externally) set
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@2190 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- Applications/Specware/bin/windows/execute.cmd
+++ Applications/Specware/bin/windows/execute.cmd
@@ -9,24 +9,24 @@ rem  Check that the needed environment variables are set:
 
 if "%SPECWARE4%"=="" (
   echo Error: environment variable SPECWARE4 not set.
-  pause
-  exit
-)
-
-if "%SWPATH%"=="" (
-  echo Error: environment variable SWPATH not set.
+  echo SPECWARE4 must be set to the path of the Specware 4 tree,
+  echo e.g. c:\users\me\specware4.
   pause
   exit
 )
 
 if "%XEMACS%"=="" (
   echo Error: environment variable XEMACS not set.
+  echo XEMACS must be set to the path of the XEmacs tree,
+  echo e.g. c:\progra~1\xemacs\xemacs-21.4.11.
   pause
   exit
 )
 
 if "%ALLEGRO%"=="" (
   echo Error: environment variable ALLEGRO not set.
+  echo ALLEGRO must be set to the path of the Allegro Common Lisp tree,
+  echo e.g. c:\progra~1\acl62.
   pause
   exit
 )
@@ -34,6 +34,8 @@ if "%ALLEGRO%"=="" (
 
 rem  Set additional environment variables that depend on the previous ones:
 
+if "%SWPATH%"=="" set SWPATH=%SPECWARE4%
+  rem set SWPATH only if unset
 set SPECWARE_BIN=%SPECWARE4%\Applications\Specware\bin\windows
 set LISP_EXECUTABLE=%ALLEGRO%\alisp.exe
 set LISP_HEAP_IMAGE=%SPECWARE_BIN%\Specware4.dxl
