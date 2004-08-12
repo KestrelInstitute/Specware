9328dfe9f8520ac39988de23810d68b3581bfe4a
Alessandro Coglio <coglio@kestrel.edu>
2004-08-12 17:04:59 +0000

    added "" so that scripts work with environment variables containing spaces (e.g. in "Program Files")
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@2282 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- Applications/Specware/bin/windows/Specware-shell.cmd
+++ Applications/Specware/bin/windows/Specware-shell.cmd
@@ -6,4 +6,4 @@ call check-and-set-environment
 
 rem  Start Specware shell within Lisp shell:
 
-%LISP_EXECUTABLE%  +t "Specware Shell" -I "%LISP_HEAP_IMAGE%" -e "(progn (setq emacs::*use-emacs-interface?* nil ) (SWShell::specware-shell t) (exit))"&
+"%LISP_EXECUTABLE%"  +t "Specware Shell" -I "%LISP_HEAP_IMAGE%" -e "(progn (setq emacs::*use-emacs-interface?* nil ) (SWShell::specware-shell t) (exit))"&
--- Applications/Specware/bin/windows/start-in-xemacs.cmd
+++ Applications/Specware/bin/windows/start-in-xemacs.cmd
@@ -6,4 +6,4 @@ call check-and-set-environment
 
 rem  Start XEmacs loading various files and performing the action given as argument (i.e. %1):
 
-%XEMACS%\i586-pc-win32\xemacs.exe -debug-init -l "%ALLEGRO%/xeli/fi-site-init" -l "%SPECWARE4%/Library/IO/Emacs/load" -f "%1" &
+"%XEMACS%\i586-pc-win32\xemacs.exe" -debug-init -l "%ALLEGRO%/xeli/fi-site-init" -l "%SPECWARE4%/Library/IO/Emacs/load" -f "%1" &
