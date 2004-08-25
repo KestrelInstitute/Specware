d866aba45e62cf7a9617c2fc7313f96453f7d434
Stephen Westfold <westfold@kestrel.edu>
2004-08-25 01:50:33 +0000

    Make cmd files windows disappear after starting specware.
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@2338 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- Applications/Specware/bin/windows/Specware-shell.cmd
+++ Applications/Specware/bin/windows/Specware-shell.cmd
@@ -6,5 +6,5 @@ call check-and-set-environment
 
 rem  Start Specware shell within Lisp shell:
 
-"%LISP_EXECUTABLE%"  +t "Specware Shell" -I "%LISP_HEAP_IMAGE%" -e "(progn (Specware::initializeSpecware-0)(SWShell::sw-shell-0))"&
+start "ignore" "%LISP_EXECUTABLE%" +t "Specware Shell" -I "%LISP_HEAP_IMAGE%" -e "(progn (Specware::initializeSpecware-0)(SWShell::sw-shell-0))"
 
