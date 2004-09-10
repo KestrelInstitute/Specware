ef1b6ec1c084f2096ce5deff36a49fe870551be1
Jim McDonald <mcdonald@kestrel.edu>
2004-09-10 08:56:51 +0000

    added test for bug 109
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@2480 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- TestSuite/Bugs/Tests.lisp
+++ TestSuite/Bugs/Tests.lisp
@@ -451,5 +451,26 @@ endspec
 
 ")
 
+ ("Bug 0109 : Declarations not printed"
+  :show   "Bug_0109/DeclsRequired"
+  :output ";;; Elaborating spec at $TESTDIR/Bug_0109/DeclsRequired
+
+spec  
+ type YesNo =  | No | Yes
+ type Affirm =  | Ok | Sure | Yes
+ op y : List String
+ op x : List Char
+ op z : Affirm
+ op g : Nat
+ op f : Integer
+
+ def y = []
+ def x = []
+ def z = Yes
+ def g = 3
+ def f = 3
+endspec
+
+")
  ;; end of tests
  )
