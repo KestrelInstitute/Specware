07373824024b052c7f8377ff0f9be0a5b218a1da
Jim McDonald <mcdonald@kestrel.edu>
2004-09-11 00:54:20 +0000

    added test cases for bugs 101 (same as for 102) and 103
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@2487 7b97033b-253e-4a5c-9b07-e25f9089a9fd

---
+++ TestSuite/Bugs/Bug_0103/NeedWFO.sw
@@ -0,0 +1,20 @@
+S = spec
+
+  op f : fa(a) {(l,i) : List a * Nat | i < length l} -> a
+
+  def f(hd::tl,i) =
+    if i = 0 then 
+      hd
+    else 
+      f(Cons(hd,tl),i)    % WRONG
+
+endspec
+
+
+O  = print obligations S
+
+P  = prove f_Obligation  in O
+P0 = prove f_Obligation0 in O
+P1 = prove f_Obligation1 in O
+
+
--- TestSuite/Bugs/Tests.lisp
+++ TestSuite/Bugs/Tests.lisp
@@ -439,6 +439,10 @@ MinusProof: Conjecture minus_def2_Obligation in ONat is Proved! using Snark.
 
 ")
 
+ ("Bug 0101 : Redundant obligations for if-then-else [see test for 0102]"
+ ;; same test as for 0102
+ )
+
  ("Bug 0102 : Extra variable in gnerated proof obligation"
   :show   "Bug_0102/ObligationsOfInteger.sw" 
   :output ";;; Elaborating obligator at $TESTDIR/Bug_0102/ObligationsOfInteger
@@ -465,6 +469,40 @@ spec
     fa(x : Integer, y : NonZeroInteger) natural?(abs x div abs y)
 endspec
 
+")
+
+ ("Bug 0103 : WFO obligation not generated"
+  :sw  "Bug_0103/NeedWFO"
+  :output ";;; Elaborating spec at $TESTDIR/Bug_0103/NeedWFO#S
+;;; Elaborating obligator at $TESTDIR/Bug_0103/NeedWFO#O
+
+spec  
+ import S
+ import /Library/Base/WFO
+ conjecture f_Obligation is [a] 
+    fa(x0 : List(a), i : Nat, tl : List(a), hd : a) 
+     i < length x0 && x0 = Cons(hd, tl) && ~(i = 0) 
+     => i < length(Cons(hd, tl))
+ conjecture f_Obligation0 is [a] 
+    ex(pred : (List(a) * Nat) * (List(a) * Nat) -> Boolean) 
+     WFO.wfo pred 
+     && fa(x0 : List(a), i : Nat, tl : List(a), hd : a) 
+         (i < length x0 && x0 = Cons(hd, tl) && ~(i = 0) 
+         => pred((Cons(hd, tl), i), (x0, i)))
+ conjecture f_Obligation1 is [a] 
+    fa(x : Nat, x0 : List(a), D : List(a) * Nat, pV2 : Nat) 
+     x < length x0 && pV2 = D.2 => embed?(Cons)(D.1)
+endspec
+
+;;; Elaborating proof-term at $TESTDIR/Bug_0103/NeedWFO#P
+P: Conjecture f_Obligation in O is Proved! using simple inequality reasoning.
+    Snark Log file: $TESTDIR/Bug_0103/Both/NeedWFO/P.log
+;;; Elaborating proof-term at $TESTDIR/Bug_0103/NeedWFO#P0
+P0: Conjecture f_Obligation0 in O is NOT proved using Snark.
+    Snark Log file: $TESTDIR/Bug_0103/Both/NeedWFO/P0.log
+;;; Elaborating proof-term at $TESTDIR/Bug_0103/NeedWFO#P1
+P1: Conjecture f_Obligation1 in O is NOT proved using Snark.
+    Snark Log file: $TESTDIR/Bug_0103/Both/NeedWFO/P1.log
 ")
 
  ("Bug 0104 : Erroneous code for non-constructive expression."
