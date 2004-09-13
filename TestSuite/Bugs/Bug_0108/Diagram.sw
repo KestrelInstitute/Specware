38256bfed636c1dd13aa717b489b1906c423b675
Jim McDonald <mcdonald@kestrel.edu>
2004-09-13 11:18:48 +0000

    make reasonable test for printing diagrams
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@2503 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- TestSuite/Bugs/Bug_0108/Diagram.sw
+++ TestSuite/Bugs/Bug_0108/Diagram.sw
@@ -1,20 +1,29 @@
 R = spec
- type A
+ type A1
+ type A2
+ type A3
+ op f : A1 * A2 -> A3
 end
 
 S = spec
- type B
+ type B1
+ type B2
+ type B3
+ op g : B1 * B2 -> B3
 end
 
 T = spec
- type C
+ type C1
+ type C2
+ type C3
+ op h : C1 * C2 -> C3
 end
 
-M = morphism R -> S {A +-> B}
-N = morphism R -> T {A +-> C}
+M = morphism R -> S {A1 +-> B1, A2 +-> B2, A3 +-> B3, f +-> g}
+N = morphism R -> T {A1 +-> C1, A2 +-> C2, A3 +-> C3, f +-> h}
 
 D = diagram {a : x -> y +-> M, b : x -> z +-> N}
 
-C = colimit D
+% C = colimit D
 
 
--- TestSuite/Bugs/Bug_0108/Tests.lisp
+++ TestSuite/Bugs/Bug_0108/Tests.lisp
@@ -3,10 +3,28 @@
 (test 
 
  ("Bug 0108 : Printing of diagram is atrocious"
-  :show   "Diagram"
-  :output ";;; Elaborating spec at $TESTDIR/Diagram
+  :show   "Diagram#D"
+  :output ";;; Elaborating diagram-term at $TESTDIR/Diagram#D
+;;; Elaborating spec-morphism at $TESTDIR/Diagram#M
+;;; Elaborating spec at $TESTDIR/Diagram#R
+;;; Elaborating spec at $TESTDIR/Diagram#S
+;;; Elaborating spec-morphism at $TESTDIR/Diagram#N
+;;; Elaborating spec at $TESTDIR/Diagram#T
 
-<something>
+diagram {a : x -> y
+         +->
+         morphism R -> S 
+          {type A1 +-> B1,
+           type A2 +-> B2,
+           type A3 +-> B3,
+           op f +-> g},
+         b : x -> z
+         +->
+         morphism R -> T 
+          {type A1 +-> C1,
+           type A2 +-> C2,
+           type A3 +-> C3,
+           op f +-> h}}
 ")
 
  )
