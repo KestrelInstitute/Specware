c1f4168c36f0f272a4de048fca9178410acebcdb
Jim McDonald <mcdonald@kestrel.edu>
2004-09-17 23:28:19 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@2539 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- TestSuite/Bugs/Bug_0111/Tests.lisp
+++ TestSuite/Bugs/Bug_0111/Tests.lisp
@@ -2,31 +2,55 @@
 
 (test 
 
- ("Bug 0111 : Capture of translated ops by var bindings [T]"
+ ("Bug 0111 : Capture of translated ops by var bindings [Once]"
   :show   "Capture#T"
   :output ";;; Elaborating spec-translation at $TESTDIR/Capture#T
 ;;; Elaborating spec at $TESTDIR/Capture#S
 
 spec  
  
- op  f : Nat -> Integer
- def f x1 = x1 + x
+ op  g : Nat -> Integer
+ def g n = let xx0 = n in 
+           xx0 + xx
  
- op  x : Nat
+ op  h : Integer -> Integer
+ def h n = let def ww0 n = n
+           in 
+           ww0 n + ww n
+ 
+ op  ff : Nat -> Integer
+ def ff xx0 = xx0 + xx
+ 
+ op  ww : Nat -> Nat
+ 
+ op  xx : Nat
+ axiom foo is fa(xx0 : Nat) xx0 = xx0 + xx
 endspec
 
 ")
 
- ("Bug 0111 : Capture of translated ops by var bindings [W]"
+ ("Bug 0111 : Capture of translated ops by var bindings [Repeated]"
   :show   "Capture#W"
   :output ";;; Elaborating spec-translation at $TESTDIR/Capture#W
 
 spec  
  
- op  f : Nat -> Integer
- def f x = x + w
+ op  g : Nat -> Integer
+ def g n = let xx0 = n in 
+           xx0 + aa
  
- op  w : Nat
+ op  h : Integer -> Integer
+ def h n = let def ww0 n = n
+           in 
+           ww0 n + bb n
+ 
+ op  ff : Nat -> Integer
+ def ff xx0 = xx0 + aa
+ 
+ op  aa : Nat
+ 
+ op  bb : Nat -> Nat
+ axiom foo is fa(xx0 : Nat) xx0 = xx0 + aa
 endspec
 
 ")
