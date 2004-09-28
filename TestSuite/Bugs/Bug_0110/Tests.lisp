08a84139e6bd143952a54e6e96fe580ebe4da7c9
Jim McDonald <mcdonald@kestrel.edu>
2004-09-28 02:13:56 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@2591 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- TestSuite/Bugs/Bug_0110/Tests.lisp
+++ TestSuite/Bugs/Bug_0110/Tests.lisp
@@ -2,13 +2,26 @@
 
 (test 
 
- ("Bug 0110 : [] read as bogus Nil"
-  :show   "BogusNil"
-  :output '(";;; Elaborating spec at $TESTDIR/BogusNil"
+ ("Bug 0110 : [] read as bogus Nil [Winner]"
+  :sw  "BogusNil#Winner"
+  :output '(";;; Elaborating spec at $TESTDIR/BogusNil#Winner"
+	    ""))
+
+ ("Bug 0110 : [] read as bogus Nil [Loser]"
+  :sw "BogusNil#Loser"
+  :output '(";;; Elaborating spec at $TESTDIR/BogusNil#Loser"
 	    "Errors in $TESTDIR/BogusNil.sw"
-	    "3.18-3.19	: Could not match type constraint"
-	    "                  [] of type  | Cons mtv%metafy%11 * List(mtv%metafy%11) | Nil"
-	    "          with expected type AA"
+	    "14.21-14.22	: Could not match type constraint"
+	    "                  [] of type List(mtv%parser%12)"
+	    "          with expected type Bogus"
+	    ""))
+
+ ("Bug 0110 : [] read as bogus Nil [Loser2]"
+  :sw "BogusNil#Loser2"
+  :output '(";;; Elaborating spec at $TESTDIR/BogusNil#Loser2"
+	    "Errors in $TESTDIR/BogusNil.sw"
+	    "20.1-20.14	: Incomplete type for op loser:"
+	    "mtv%parser%14"
 	    ""))
 
  )
