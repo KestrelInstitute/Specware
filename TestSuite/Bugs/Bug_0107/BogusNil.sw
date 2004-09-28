ef54c962bd756208aa8f0357223501202b56d2cf
Jim McDonald <mcdonald@kestrel.edu>
2004-09-28 04:47:02 +0000

    add test for bogus cons
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@2595 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- TestSuite/Bugs/Bug_0107/BogusNil.sw
+++ TestSuite/Bugs/Bug_0107/BogusNil.sw
@@ -1,4 +1,7 @@
 spec
- type NotList = | Nil | Whatever
- def b : NotList = Nil   %% Don't print as [] 
+ type NotList a = | Nil | Cons (a * NotList a)
+ def bogus_nil  : NotList Nat = Nil                                      %% Don't print as [] 
+ def bogus_cons : NotList Nat = Cons (4, Cons (5, Cons (6, bogus_nil)))  %% Don't print as [5,6] 
+ def true_nil   : List Nat  = []
+ def true_cons  : List Nat  = [1,2,3]
 endspec
--- TestSuite/Bugs/Bug_0107/Tests.lisp
+++ TestSuite/Bugs/Bug_0107/Tests.lisp
@@ -4,15 +4,24 @@
 
  ("Bug 0107 : Bogus Nil prints as []"
   :show   "BogusNil"
-  :output ";;; Elaborating spec at $TESTDIR/BogusNil
-
-spec  
- type NotList =  | Nil | Whatever
- 
- op  b : NotList
- def b = Nil
-endspec
-
-")
+  :output '(";;; Elaborating spec at $TESTDIR/BogusNil"
+	    ""
+	    "spec  "
+	    " type NotList(a) =  | Cons a * NotList(a) | Nil"
+	    " "
+	    " op  bogus_nil : NotList(Nat)"
+	    " def bogus_nil = Nil"
+	    " "
+	    " op  true_nil : List(Nat)"
+	    " def true_nil = []"
+	    " "
+	    " op  true_cons : List(Nat)"
+	    " def true_cons = [1, 2, 3]"
+	    " "
+	    " op  bogus_cons : NotList(Nat)"
+	    " def bogus_cons = Cons(4, Cons(5, Cons(6, bogus_nil)))"
+	    "endspec"
+	    ""
+	    ""))
 
  )
