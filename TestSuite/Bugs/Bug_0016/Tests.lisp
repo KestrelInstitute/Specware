8d9bf3deb863cf2913d608cdf5b9864ce24d4f8c
Jim McDonald <mcdonald@kestrel.edu>
2004-09-30 22:58:50 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@2603 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- TestSuite/Bugs/Bug_0016/Tests.lisp
+++ TestSuite/Bugs/Bug_0016/Tests.lisp
@@ -4,10 +4,14 @@
 
  ("Bug 0016 : An incorrect SWPATH produces no error or warning"
   :path "/loser/loser/loser"
-  :output '("Warning: Directory does not exist: /loser/loser/loser"
+  :output '((:alternatives 
+	     "Warning: Directory does not exist: /loser/loser/loser"
+	     "Warning: Directory does not exist: /loser/loser/loser/")
 	    "Keeping old path:"
 	    (:alternatives 
-	     ("$SPECWARE:/:$SPECWARE/")
-	     ("$SPECWARE:.:$SPECWARE/"))))
+	     "$SPECWARE:/:$SPECWARE/"
+	     "$SPECWARE:.:$SPECWARE/"
+	     "$SPECWARE/:./"
+	     )))
 
  )
