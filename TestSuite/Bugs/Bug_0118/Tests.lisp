55c415056fe4480dab1991eaa946a2fc99939716
Jim McDonald <mcdonald@kestrel.edu>
2004-10-03 10:45:28 +0000

    include more cases, inspired by ProverBase/Functions.sw
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@2608 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- TestSuite/Bugs/Bug_0118/Tests.lisp
+++ TestSuite/Bugs/Bug_0118/Tests.lisp
@@ -3,13 +3,15 @@
 (test 
 
  ("Bug 0118 : Equivalence of Pi types failing"
-  :show   "EquivSorts#C"
+  :show   "EquivSorts#D"
   :output '(
-	    ";;; Elaborating spec at $TESTDIR/EquivSorts#C"
+	    ";;; Elaborating spec at $TESTDIR/EquivSorts#D"
 	    ";;; Elaborating spec at $TESTDIR/EquivSorts#A"
 	    ";;; Elaborating spec at $TESTDIR/EquivSorts#B"
+	    ";;; Elaborating spec at $TESTDIR/EquivSorts#C"
 	    ""
 	    "spec  "
+	    " import C"
 	    " import B"
 	    " import A"
 	    "endspec"
