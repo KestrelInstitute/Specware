d8eb2a33fd008c74e379bb6ddd19fd506819c47b
Stephen Westfold <westfold@kestrel.edu>
2004-11-13 01:15:37 +0000

    Adjust output of test case to new behavior.
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@2745 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- TestSuite/Bugs/Bug_0099/Tests.lisp
+++ TestSuite/Bugs/Bug_0099/Tests.lisp
@@ -4,15 +4,14 @@
 
  ("Bug 0099 : Extra 'let' in generated proof obligation"
   :show   "MinusOb#MinusProof" 
-  :output ";;; Elaborating proof-term at $TESTDIR/MinusOb#MinusProof
-;;; Elaborating obligator at $TESTDIR/MinusOb#ONat
-;; ensure-directories-exist: creating $TESTDIR/Snark/MinusOb/MinusProof.log
-;; Directory $TESTDIR/Snark/ does not exist, will create.
-;; Directory $TESTDIR/Snark/MinusOb/ does not exist, will create.
-MinusProof: Conjecture minus_def2_Obligation in ONat is Proved! using Snark.
-    Snark Log file: $TESTDIR/Snark/MinusOb/MinusProof.log
-
-
-")
+  :output '(
+	    ";;; Elaborating proof-term at $TESTDIR/MinusOb#MinusProof"
+	    ";;; Elaborating obligator at $TESTDIR/MinusOb#ONat"
+	    "    Expanded spec file: $TESTDIR/Snark/MinusOb/MinusProof.sw"
+	    "    Snark Log file: $TESTDIR/Snark/MinusOb/MinusProof.log"
+	    "MinusProof: Conjecture minus_def2_Obligation in ONat is Proved! using Snark."
+	    ""
+	    ""
+	    ""))
 
  )
