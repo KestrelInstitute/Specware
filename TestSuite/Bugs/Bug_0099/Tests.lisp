42e72b856a6b322441c379de9f024bd0c342ab06
Jim McDonald <mcdonald@kestrel.edu>
2004-11-15 19:30:17 +0000

    anticipate some optional output
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@2754 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- TestSuite/Bugs/Bug_0099/Tests.lisp
+++ TestSuite/Bugs/Bug_0099/Tests.lisp
@@ -7,8 +7,31 @@
   :output '(
 	    ";;; Elaborating proof-term at $TESTDIR/MinusOb#MinusProof"
 	    ";;; Elaborating obligator at $TESTDIR/MinusOb#ONat"
+	    (:optional
+	     ";;; Elaborating spec at $SPECWARE/Library/Base/WFO"
+	     ";;; Elaborating spec at $SPECWARE/Library/ProverBase/Top"
+	     ";;; Elaborating spec at $SPECWARE/Library/ProverBase/Char"
+	     ";;; Elaborating spec at $SPECWARE/Library/ProverBase/Compare"
+	     ";;; Elaborating spec at $SPECWARE/Library/ProverBase/Functions"
+	     ";;; Elaborating spec at $SPECWARE/Library/ProverBase/Integer"
+	     ";;; Elaborating spec at $SPECWARE/Library/ProverBase/List"
+	     ";;; Elaborating spec at $SPECWARE/Library/ProverBase/Nat"
+	     ";;; Elaborating spec at $SPECWARE/Library/ProverBase/Option"
+	     ";;; Elaborating spec at $SPECWARE/Library/ProverBase/String"
+	     ";;; Elaborating spec at $SPECWARE/Library/ProverBase/System"
+	     ";;; Elaborating spec at $SPECWARE/Library/Base/ProverRewrite")
+	    (:optional
+	     ";; ensure-directories-exist: creating $TESTDIR/Snark/MinusOb/MinusProof.log"
+	     ";; Directory $TESTDIR/Snark/ does not exist, will create."
+	     ";; Directory $TESTDIR/Snark/MinusOb/ does not exist, will create.")
+	    (:optional
+	     ";;; Elaborating spec at $SPECWARE/Library/Base/ProverBase")
 	    "    Expanded spec file: $TESTDIR/Snark/MinusOb/MinusProof.sw"
 	    "    Snark Log file: $TESTDIR/Snark/MinusOb/MinusProof.log"
+	    (:optional 
+	     ";; ensure-directories-exist: creating $TESTDIR/Snark/MinusOb/MinusProof.log"
+	     ";; Directory $TESTDIR/Snark/ does not exist, will create."
+	     ";; Directory $TESTDIR/Snark/MinusOb/ does not exist, will create.")
 	    "MinusProof: Conjecture minus_def2_Obligation in ONat is Proved! using Snark."
 	    ""
 	    ""
--- TestSuite/Bugs/Bug_0125/Tests.lisp
+++ TestSuite/Bugs/Bug_0125/Tests.lisp
@@ -6,11 +6,28 @@
   :sw "PosNat#p"
   :output '(";;; Elaborating proof-term at $TESTDIR/PosNat#p"
 	    ";;; Elaborating spec at $TESTDIR/PosNat#s"
+	    (:optional
+	     ";;; Elaborating spec at $SPECWARE/Library/ProverBase/Top"
+	     ";;; Elaborating spec at $SPECWARE/Library/ProverBase/Char"
+	     ";;; Elaborating spec at $SPECWARE/Library/ProverBase/Compare"
+	     ";;; Elaborating spec at $SPECWARE/Library/ProverBase/Functions"
+	     ";;; Elaborating spec at $SPECWARE/Library/ProverBase/Integer"
+	     ";;; Elaborating spec at $SPECWARE/Library/ProverBase/List"
+	     ";;; Elaborating spec at $SPECWARE/Library/ProverBase/Nat"
+	     ";;; Elaborating spec at $SPECWARE/Library/ProverBase/Option"
+	     ";;; Elaborating spec at $SPECWARE/Library/ProverBase/String"
+	     ";;; Elaborating spec at $SPECWARE/Library/ProverBase/System"
+	     ";;; Elaborating spec at $SPECWARE/Library/Base/ProverRewrite"
+	     ";; ensure-directories-exist: creating $TESTDIR/Snark/PosNat/p.log"
+	     ";; Directory $TESTDIR/Snark/ does not exist, will create."
+	     ";; Directory $TESTDIR/Snark/PosNat/ does not exist, will create."
+	     ";;; Elaborating spec at $SPECWARE/Library/Base/ProverBase")
 	    (:optional ";; ensure-directories-exist: creating $TESTDIR/Snark/PosNat/p.log")
 	    (:optional ";; Directory $TESTDIR/Snark/ does not exist, will create.")
 	    (:optional ";; Directory $TESTDIR/Snark/PosNat/ does not exist, will create.")
-	    "p: Conjecture c in s is NOT proved using Snark."
+	    "    Expanded spec file: $TESTDIR/Snark/PosNat/p.sw"
 	    "    Snark Log file: $TESTDIR/Snark/PosNat/p.log"
+	    "p: Conjecture c in s is NOT proved using Snark."
 	    ""))
 
  )
