0f958bfc0569d15a9eb7401fcf2f0cced877fff0
Jim McDonald <mcdonald@kestrel.edu>
2004-12-02 03:04:59 +0000

    added test-specware4-subdir, revised test-specware4-bugs and test-specware4-bugs to use it
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@2819 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- Applications/Specware/Tests/test-specware4.lisp
+++ Applications/Specware/Tests/test-specware4.lisp
@@ -15,19 +15,27 @@
   log-file-name)
 
 
+(defun test-specware4-subdir (subdir
+			      &optional (log-file-name
+					 (multiple-value-bind (ss mm hh dd mm yy)
+					     (decode-universal-time (get-universal-time))
+					   (format nil "/tmp/test-specware4-~D-~D-~D-~D-~D-~D"
+						   yy mm dd hh mm ss))))
+  (format t "~&Testing Specware4, results to ~A ...~%" log-file-name)
+  (with-open-file (s log-file-name :direction :output) 
+    (let ((*standard-output* s)
+	  (*error-output* s)
+	  (test-dir (format nil "~A/TestSuite/~A/" (specware::getenv "SPECWARE4") subdir)))
+      (specware-test::run-test-directories-rec-fn (list test-dir))))
+  (format t "~&Done.~%")
+  log-file-name)
+
 (defun test-specware4-bugs (&optional (log-file-name
 				       (multiple-value-bind (ss mm hh dd mm yy)
 					   (decode-universal-time (get-universal-time))
 					 (format nil "/tmp/test-specware4-~D-~D-~D-~D-~D-~D"
 						 yy mm dd hh mm ss))))
-  (format t "~&Testing Specware4, results to ~A ...~%" log-file-name)
-  (with-open-file (s log-file-name :direction :output) 
-    (let ((*standard-output* s)
-	  (*error-output* s)
-	  (test-dir (format nil "~A/TestSuite/Bugs/" (specware::getenv "SPECWARE4"))))
-      (specware-test::run-test-directories-rec-fn (list test-dir))))
-  (format t "~&Done.~%")
-  log-file-name)
+  (test-specware4-subdir "Bugs" log-file-name))
 
 
 (defun test-specware4-bug (bug-number
@@ -36,15 +44,8 @@
 					  (decode-universal-time (get-universal-time))
 					(format nil "/tmp/test-specware4-~D-~D-~D-~D-~D-~D"
 						yy mm dd hh mm ss))))
-  (format t "~&Testing Specware4, results to ~A ...~%" log-file-name)
-  (with-open-file (s log-file-name :direction :output) 
-    (let ((*standard-output* s)
-	  (*error-output* s)
-	  (test-dir (format nil "~A/TestSuite/Bugs/Bug_~4,'0D" 
-			    (specware::getenv "SPECWARE4") 
-			    bug-number)))
-      (specware-test::run-test-directories-rec-fn (list test-dir))))
-  (format t "~&Done.~%")
-  log-file-name)
+  (let ((subdir (format nil "Bugs/Bug_~4,'0D" bug-number)))
+    (test-specware4-subdir subdir log-file-name)))
+
 
 
