4c3cfa2f287ece17d85dba5702460cff49faddff
Jim McDonald <mcdonald@kestrel.edu>
2004-12-10 06:45:01 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@2863 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- TestSuite/Bugs/Bug_0146/Tests.lisp
+++ TestSuite/Bugs/Bug_0146/Tests.lisp
@@ -2,10 +2,9 @@
 
 (test 
 
- ("Bug 0146 : Erroneous code for non-constructive expression."
+ ("Bug 0146 : Code for nonconstructive definitions is generated."
   :swl  "NonConstructive"
-  :output '(";;; Code for nonconstructive definitions is generated."
-	    "<some kind of error message>"
+  :output '("<some kind of error message>"
 	    (:optional ";; ensure-directories-exist: creating $TESTDIR/lisp/NonConstructive.lisp")
 	    (:optional ";; Directory $TESTDIR/lisp/ does not exist, will create.")
 	    ""))
