00a0126d29cf72721edda705df74693f7a873d0f
Jim McDonald <mcdonald@kestrel.edu>
2005-03-08 02:28:26 +0000

    ilisp interface mysteriously hangs, so revert back to allegro interface for now
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@3008 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- Applications/Specware/bin/windows/start-in-xemacs.cmd
+++ Applications/Specware/bin/windows/start-in-xemacs.cmd
@@ -6,4 +6,7 @@ call check-and-set-environment
 
 rem  Start XEmacs loading various files and performing the action given as argument (i.e. %1):
 
-start "ignore" "%XEMACS%\i586-pc-win32\xemacs.exe" -debug-init -l "%SPECWARE4%/Library/IO/Emacs/load-ilisp" -f "%1" &
+rem following attempt to use ilisp interface doesn't work, for some mysterious reason:
+rem start "ignore" "%XEMACS%\i586-pc-win32\xemacs.exe" -debug-init -l "%SPECWARE4%/Library/IO/Emacs/load-ilisp" -f "%1" &
+
+start "ignore" "%XEMACS%\i586-pc-win32\xemacs.exe" -debug-init -l "%ALLEGRO%/xeli/fi-site-init" -l "%SPECWARE4%/Library/IO/Emacs/load" -f "%1" &
