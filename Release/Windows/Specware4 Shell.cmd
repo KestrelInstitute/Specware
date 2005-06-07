9baa2d664fc9637083e75fe24617bca74547bb31
yunmei <yunmei@kestrel.edu>
2005-06-07 18:45:48 +0000

    Updated so they get SPECWARE4 location from where they are launched.
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@3156 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- Release/Windows/Specware4 Shell.cmd
+++ Release/Windows/Specware4 Shell.cmd
@@ -1,11 +1,20 @@
 echo off
-rem These paths are set by the Specware installer.  SWPATH may be changed if your project necessitates it.
-set SPECWARE4=
-set SWPATH=
+
+set SPECWARE4=%CD%
+echo SPECWARE4=%SPECWARE4%
+IF NOT EXIST "%SPECWARE4%\Specware4.exe" (
+  echo Cannot find Specware Executable %SPECWARE4%\Specware4.exe
+  Pause
+  Exit
+)
+
+set SWPATH=C:/
+
+echo SPECWARE4=%SPECWARE4%
 
 set "LISP_EXECUTABLE=%SPECWARE4%\Specware4.exe"
 set "LISP_HEAP_IMAGE=%SPECWARE4%\Specware4.dxl"
-set LISP_DIRECTORY=%Specware4%/
+set LISP_DIRECTORY="%Specware4%/"
 cd "%Specware4%"
 
-start "ignore" %LISP_EXECUTABLE%  +t "Specware Shell" +s "%SPECWARE4%\StartShell.lisp" -I "%LISP_HEAP_IMAGE%" -e "(setq emacs::*use-emacs-interface?* nil)"
+start "ignore" "%LISP_EXECUTABLE%"  +t "Specware Shell" +s "%SPECWARE4%\StartShell.lisp" -I "%LISP_HEAP_IMAGE%" -e "(setq emacs::*use-emacs-interface?* nil)"
--- Release/Windows/Specware4.cmd
+++ Release/Windows/Specware4.cmd
@@ -1,8 +1,43 @@
 echo off
-rem These paths are set by the Specware installer.  SWPATH may be changed if your project necessitates it.
-set SPECWARE4=
-set SWPATH=
-set XEMACS_EXE=
+
+set SPECWARE4=%CD%
+echo SPECWARE4=%SPECWARE4%
+IF NOT EXIST "%SPECWARE4%\Specware4.exe" (
+  echo Cannot find Specware Executable %SPECWARE4%\Specware4.exe
+  Pause
+  Exit
+)
+
+set SWPATH=C:/
+
+rem This looks for XEmacs current directory in c:\progra~1\XEmacs\.
+rem If it is somewhere else you can set it explicitly.
+
+rem set XEMACS=...
+
+IF "%XEMACS%"=="" (
+   FOR /F "tokens=*" %%i IN ('dir /B c:\progra~1\XEmacs\xemacs-2*') DO set XEMACS=c:\progra~1\XEmacs\%%i
+) ELSE (
+IF NOT EXIST %XEMACS%\i586-pc-win32\xemacs.exe (
+   FOR /F "tokens=*" %%i IN ('dir /B c:\progra~1\XEmacs\xemacs-2*') DO set XEMACS=c:\progra~1\XEmacs\%%i
+)
+)
+
+IF "%XEMACS%"=="" (
+  echo Cannot find XEmacs in C:\Program Files\XEmacs
+  echo Set XEMACS explicitly if it is installed elsewhere.
+  Pause
+  Exit
+)
+
+echo XEMACS=%XEMACS%
+
+set XEMACS_EXE=%XEMACS%\i586-pc-win32\xemacs.exe
+IF NOT EXIST %XEMACS_EXE% (
+  echo Cannot find XEmacs Executable: %XEMACS_EXE%
+  Pause
+  Exit
+)
 
 rem The following environment variables are used within the emacs
 rem interface to find the executable and the Specware world to run. Note
@@ -19,7 +54,7 @@ rem specware.
 
 set "LISP_EXECUTABLE=%SPECWARE4%\Specware4.exe"
 set "LISP_HEAP_IMAGE=%SPECWARE4%\Specware4.dxl"
-set LISP_DIRECTORY=%Specware4%/
+set LISP_DIRECTORY="%Specware4%/"
 cd "%Specware4%"
 
 start "ignore" "%XEMACS_EXE%" -l "%SPECWARE4%\Library\IO\Emacs\xeli\fi-site-init" -l "%SPECWARE4%\Library\IO\Emacs\load" -f run-specware4
