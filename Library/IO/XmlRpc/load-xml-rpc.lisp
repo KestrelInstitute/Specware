baceb4845e4e242522be7b774f44e0d101ab38eb
ma <ma@kestrel.edu>
2005-09-09 06:25:32 +0000

    added code to enable xml-rpc for allegro
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@3401 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- Library/IO/XmlRpc/load-xml-rpc.lisp
+++ Library/IO/XmlRpc/load-xml-rpc.lisp
@@ -1,25 +1,30 @@
 (format t "loading xml-rpc...~%")
 
-(defpackage :s-xml)
-(defpackage :s-xml-rpc)
+#+allegro
+(progn
+  (in-package :common-lisp-user)
 
-;(defvar Specware4 (specware::getenv "SPECWARE4"))
+  (defvar *xml-rpc-server-id*)
+  (require :xml-rpc)
+  (defun start-xml-rpc-server (port)
+    (let ((start-args (list :port port)))
+      (progn
+	(setq *xml-rpc-server-id*
+	      (net.xml-rpc:make-xml-rpc-server
+	       :start start-args :enable t :introspect t))
+	(net.xml-rpc:export-standard-xml-rpc-methods *xml-rpc-server-id*))))
+)
 
-;(defvar *xml-rpc-dir* (concatenate 'string Specware4 "/Library/IO/XmlRpc"))
+#+openmcl
+(progn
+  (defpackage :s-xml)
+  (defpackage :s-xml-rpc)
 
-;(compile-and-load-lisp-file (concatenate 'string *xml-rpc-dir* "/s-xml/package"))
-;(compile-and-load-lisp-file (concatenate 'string *xml-rpc-dir* "/s-xml/xml"))
-;(compile-and-load-lisp-file (concatenate 'string *xml-rpc-dir* "/s-xml-rpc/base64"))
-;(compile-and-load-lisp-file (concatenate 'string *xml-rpc-dir* "/s-xml-rpc/package"))
-;(compile-and-load-lisp-file (concatenate 'string *xml-rpc-dir* "/s-xml-rpc/sysdeps"))
-;(compile-and-load-lisp-file (concatenate 'string *xml-rpc-dir* "/s-xml-rpc/xml-rpc"))
-;(compile-and-load-lisp-file (concatenate 'string *xml-rpc-dir* "/s-xml-rpc/extensions"))
+  (in-package :common-lisp-user)
 
-
-(in-package :common-lisp-user)
-
-(defun start-xml-rpc-server (port) 
-  (s-xml-rpc:start-xml-rpc-server :port port)
-  (format t ";;; XML-RPC server listening at port ~A~%" port)
+  (defun start-xml-rpc-server (port) 
+    (s-xml-rpc:start-xml-rpc-server :port port)
+    (format t ";;; XML-RPC server listening at port ~A~%" port)
+    )
 )
 
