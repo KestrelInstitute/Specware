4fb7e5723d8ef946205de523d0aac3e02c7a18fa
coredev <coredev@kestrel.edu>
2006-01-26 16:39:32 +0000

    no message
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@3678 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- Release/BuildScripts/Build_Specware_Windows_ACL_Distribution.cmd
+++ Release/BuildScripts/Build_Specware_Windows_ACL_Distribution.cmd
@@ -26,7 +26,7 @@ set "LISP_HEAP_IMAGE=c:\Program Files\acl70\alisp.dxl"
 rem %SPECWARE4% may have forward slash (e.g. "C:/") where cd demands backslash
 rem cd "%SPECWARE4%\Release\BuildScripts\"
 
-start "ignore" "%LISP_EXECUTABLE%"  +t "Build Specware Distribution" -L "BuildSpecwareDistribution.lisp" -e '(progn (user::build-specware-release 4 1 4 "%SPECWARE4%/" "%DISTRIBUTION%/" %VERBOSE%) (exit 0))' -I "%LISP_HEAP_IMAGE%" 
+start "ignore" "%LISP_EXECUTABLE%"  +t "Build Specware Distribution" -L "BuildSpecwareDistribution.lisp" -e '(progn (user::build-specware-release 4 1 4 "%SPECWARE4%/" "%DISTRIBUTION%/" %VERBOSE%) (sleep 10) (exit 0))' -I "%LISP_HEAP_IMAGE%" 
 
 
 
