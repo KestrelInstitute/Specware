83b8da87f8937c835a6eaf3079d92d6183c670f2
coredev <coredev@kestrel.edu>
2006-02-16 01:37:21 +0000

    no message
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@3733 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- Release/BuildScripts/GatherSpecwareComponents.lisp
+++ Release/BuildScripts/GatherSpecwareComponents.lisp
@@ -313,8 +313,10 @@
 
     (let* ((specware-xeli-dir (extend-directory source-dir "xeli"))
 	   (source-xeli-dir   (if (null (generic-directory specware-xeli-dir))
-				  "/usr/local/acl/acl70/xeli/"
-				specware-xeli-dir)))
+				  #+Linux     "/usr/local/acl/acl70/xeli/"
+				  #+MSWindows "C:\\Program Files\\acl70\\xeli\\"
+				  #-(or Linux MSWindows) (error "Unknown OS for xeli")
+				  specware-xeli-dir)))
       (copy-dist-directory source-xeli-dir
 			   (extend-directory franz-dir  "xeli")))
 
