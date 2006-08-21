2c4930ad672e6abc570fd43048da41c5d999fc95
Jim McDonald <mcdonald@kestrel.edu>
2006-08-21 23:49:23 +0000

    Put double-quotes around filenames to avoid problems with environment variables expanding into directories containing spaces, periods, etc.
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@3906 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- Applications/Specware/bin/windows/check-and-set-environment.cmd
+++ Applications/Specware/bin/windows/check-and-set-environment.cmd
@@ -1,43 +1,44 @@
 @echo off
 
-
 rem  Check that the needed environment variables are set:
+rem  In general, enclose filenames in double-quotes, since enviroment 
+rem  variables could expand to dirctories containing spaces, periods, 
+rem  and other nasty characters.
 
 if "%SPECWARE4%"=="" (
-  echo Error: environment variable SPECWARE4 not set.
-  echo SPECWARE4 must be set to the path of the Specware 4 tree,
-  echo e.g. c:\users\me\specware4.
+  echo  Error: environment variable SPECWARE4 not set.
+  echo  SPECWARE4 must be set to the path of the Specware tree,
+  echo  e.g. C:\Specware4.
   pause
   exit
 )
 
 if "%XEMACS%"=="" (
-  echo Error: environment variable XEMACS not set.
-  echo XEMACS must be set to the path of the XEmacs tree,
-  echo e.g. c:\progra~1\xemacs\xemacs-21.4.11.
+  echo  Error: environment variable XEMACS not set.
+  echo  XEMACS must be set to the path of the XEmacs tree,
+  echo  e.g. C:\Progra~1\xemacs\xemacs-21.4.18.
   pause
   exit
 )
 
 if "%ALLEGRO%"=="" (
-  echo Error: environment variable ALLEGRO not set.
-  echo ALLEGRO must be set to the path of the Allegro Common Lisp tree,
-  echo e.g. c:\progra~1\acl62.
+  echo  Error: environment variable ALLEGRO not set.
+  echo  ALLEGRO must be set to the path of the Allegro Common Lisp tree,
+  echo  e.g. C:\Progra~1\acl62.
   pause
   exit
 )
 
-
 rem  Set additional environment variables that depend on the previous ones:
 
-if "%SWPATH%"=="" set SWPATH=C:/
-  rem set SWPATH only if unset
-set SPECWARE_BIN=%SPECWARE4%\Applications\Specware\bin\windows
-set LISP_EXECUTABLE=%ALLEGRO%\alisp.exe
-set LISP_HEAP_IMAGE=%SPECWARE_BIN%\Specware4.dxl
-set LISP_DIRECTORY=%Specware4%/
+if "%SWPATH%"=="" set SWPATH="C:/"
+rem set SWPATH only if unset
 
+set SPECWARE_BIN="%SPECWARE4%\Applications\Specware\bin\windows"
+set LISP_EXECUTABLE="%ALLEGRO%\alisp.exe"
+set LISP_HEAP_IMAGE="%SPECWARE_BIN%\Specware4.dxl"
+set LISP_DIRECTORY="%Specware4%/"
 
-rem  Move to Specware 4 main directory:
+rem  Move to Specware4 main directory:
 
 cd "%SPECWARE4%"
