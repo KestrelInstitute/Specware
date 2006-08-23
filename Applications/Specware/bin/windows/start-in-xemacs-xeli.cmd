9b7bd66dc8571853a430cf6af9dd0045daad77b9
Jim McDonald <mcdonald@kestrel.edu>
2006-08-23 02:36:02 +0000

    cosmetic
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@3914 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- Applications/Specware/bin/windows/start-in-xemacs-xeli.cmd
+++ Applications/Specware/bin/windows/start-in-xemacs-xeli.cmd
@@ -11,7 +11,4 @@ call check-and-set-environment
 
 rem  Start XEmacs loading various files and performing the action given as argument (i.e. %1):
 
-rem following attempt to use ilisp interface doesn't work, for some mysterious reason:
-rem start "ignore" "%XEMACS%\i586-pc-win32\xemacs.exe" -debug-init -l "%SPECWARE4%/Library/IO/Emacs/load-ilisp" -f "%1" &
-
-start "ignore" %MAYBE_WAIT% "%XEMACS%\i586-pc-win32\xemacs.exe" -debug-init -l "%ALLEGRO%/xeli/fi-site-init" -l "%SPECWARE4%/Library/IO/Emacs/load" -f "%1"
+start "ignore" %MAYBE_WAIT% "%XEMACS%\i586-pc-win32\xemacs.exe" -l "%ALLEGRO%/xeli/fi-site-init" -l "%SPECWARE4%/Library/IO/Emacs/load" -f "%1"
