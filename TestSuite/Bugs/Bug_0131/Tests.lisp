3a4d4be7f0d458962c8b5ddcdfac352c7f43db9b
Jim McDonald <mcdonald@kestrel.edu>
2006-10-08 10:59:14 +0000

    revise to expect latest output from prover
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@3979 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- TestSuite/Bugs/Bug_0131/Tests.lisp
+++ TestSuite/Bugs/Bug_0131/Tests.lisp
@@ -6,14 +6,37 @@
   :sw "Option#P"
   :output '(";;; Elaborating obligator at $TESTDIR/Option#P"
 	    ";;; Elaborating spec at $TESTDIR/Option#O"
-	    ";;; Elaborating proof-term at $TESTDIR/Option#P"
-	    (:optional ";;; Elaborating spec at $SPECWARE/Provers/ProofChecker/Libs/Logic")
-	    (:optional ";; ensure-directories-exist: creating $TESTDIR/Snark/Option/P.log")
+	    (:optional ";;; Elaborating spec at $SPECWARE/Library/Base/WFO")
+	    (:optional ";;; Elaborating proof-term at $TESTDIR/.")
+	    (:optional ";;; Elaborating spec at $SPECWARE/Library/ProverBase/Top")
+	    (:optional ";;; Elaborating spec at $SPECWARE/Library/ProverBase/Char")
+	    (:optional ";;; Elaborating spec at $SPECWARE/Library/ProverBase/Compare")
+	    (:optional ";;; Elaborating spec at $SPECWARE/Library/ProverBase/Functions")
+	    (:optional ";;; Elaborating spec at $SPECWARE/Library/ProverBase/Integer")
+	    (:optional ";;; Elaborating spec at $SPECWARE/Library/ProverBase/List")
+	    (:optional ";;; Elaborating spec at $SPECWARE/Library/ProverBase/Nat")
+	    (:optional ";;; Elaborating spec at $SPECWARE/Library/ProverBase/Option")
+	    (:optional ";;; Elaborating spec at $SPECWARE/Library/ProverBase/String")
+	    (:optional ";;; Elaborating spec at $SPECWARE/Library/ProverBase/System")
+	    (:optional ";;; Elaborating spec at $SPECWARE/Library/Base/ProverRewrite")
+	    (:optional ";; ensure-directories-exist: creating $TESTDIR/Snark/..log")
 	    (:optional ";; Directory $TESTDIR/Snark/ does not exist, will create.")
-	    (:optional ";; Directory $TESTDIR/Snark/Option/ does not exist, will create.")
-	    "    Expanded spec file: $TESTDIR/Snark/Option/P.sw"
-	    "    Snark Log file: $TESTDIR/Snark/Option/P.log"
-	    "P: Conjecture PFunctions.o_Obligation is Proved! using Snark*."
+	    (:optional ";;; Elaborating spec at $SPECWARE/Library/Base/ProverBase")
+	    (:optional "    Expanded spec file: $TESTDIR/Snark/..sw")
+	    (:optional "    Snark Log file: $TESTDIR/Snark/..log")
+	    (:alternatives
+	     "Conjecture PFunctions.o_Obligation_fn_precond is Proved! using Snark in 1.0 seconds."
+	     "Conjecture PFunctions.o_Obligation_fn_precond is Proved! using Snark in 1.1 seconds."
+	     "Conjecture PFunctions.o_Obligation_fn_precond is Proved! using Snark in 1.2 seconds."
+	     "Conjecture PFunctions.o_Obligation_fn_precond is Proved! using Snark in 1.3 seconds."
+	     "Conjecture PFunctions.o_Obligation_fn_precond is Proved! using Snark in 1.4 seconds."
+	     "Conjecture PFunctions.o_Obligation_fn_precond is Proved! using Snark in 1.5 seconds."
+	     "Conjecture PFunctions.o_Obligation_fn_precond is Proved! using Snark in 1.6 seconds."
+	     "Conjecture PFunctions.o_Obligation_fn_precond is Proved! using Snark in 1.7 seconds."
+	     "Conjecture PFunctions.o_Obligation_fn_precond is Proved! using Snark in 1.8 seconds."
+	     "Conjecture PFunctions.o_Obligation_fn_precond is Proved! using Snark in 1.9 seconds."
+	     "Conjecture PFunctions.o_Obligation_fn_precond is Proved! using Snark in 2.0 seconds."
+	     )
 	    ""))
 
  )
