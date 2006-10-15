373c181cebd88c467166944a2b175940c0a056bf
Jim McDonald <mcdonald@kestrel.edu>
2006-10-15 05:38:31 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@4007 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- TestSuite/Bugs/Bug_0103/Tests.lisp
+++ TestSuite/Bugs/Bug_0103/Tests.lisp
@@ -17,13 +17,28 @@
 	    " "
 	    " def f (hd :: tl, i) = if i = 0 then hd else f(Cons(hd, tl), i)"
 	    "endspec"
-	    ""
+	    (:optional "")
+	    (:optional "")
+	    ";;; Elaborating proof-term at $TESTDIR/NeedWFO#P"
+	    (:optional ";;; Elaborating spec at $SPECWARE/Library/ProverBase/Top")
+	    (:optional ";;; Elaborating spec at $SPECWARE/Library/ProverBase/Char")
+	    (:optional ";;; Elaborating spec at $SPECWARE/Library/ProverBase/Compare")
+	    (:optional ";;; Elaborating spec at $SPECWARE/Library/ProverBase/Functions")
+	    (:optional ";;; Elaborating spec at $SPECWARE/Library/ProverBase/Integer")
+	    (:optional ";;; Elaborating spec at $SPECWARE/Library/ProverBase/List")
+	    (:optional ";;; Elaborating spec at $SPECWARE/Library/ProverBase/Nat")
+	    (:optional ";;; Elaborating spec at $SPECWARE/Library/ProverBase/Option")
+	    (:optional ";;; Elaborating spec at $SPECWARE/Library/ProverBase/String")
+	    (:optional ";;; Elaborating spec at $SPECWARE/Library/ProverBase/System")
+	    (:optional ";;; Elaborating spec at $SPECWARE/Library/Base/ProverRewrite")
 	    (:optional ";; ensure-directories-exist: creating $TESTDIR/Snark/NeedWFO/P.log")
 	    (:optional ";; Directory $TESTDIR/Snark/ does not exist, will create.")
 	    (:optional ";; Directory $TESTDIR/Snark/NeedWFO/ does not exist, will create.")
-	    ";;; Elaborating proof-term at $TESTDIR/NeedWFO#P"
+	    (:optional ";;; Elaborating spec at $SPECWARE/Library/Base/ProverBase")
 	    "    Expanded spec file: $TESTDIR/Snark/NeedWFO/P.sw"
 	    "    Snark Log file: $TESTDIR/Snark/NeedWFO/P.log"
-	    "P: Conjecture f_Obligation_fn_precond in O is NOT proved using Snark*."
-	    ""))
+	    "P: Conjecture f_Obligation_fn_precond in O is NOT proved*"
+	    (:optional "")
+	    (:optional "")
+	    ))
  )
