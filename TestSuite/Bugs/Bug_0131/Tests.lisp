c89260b0d6fa7b2ac5e99ba1dfddf9b09e5f7eea
Jim McDonald <mcdonald@kestrel.edu>
2006-11-14 10:01:57 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@4110 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- TestSuite/Bugs/Bug_0131/Tests.lisp
+++ TestSuite/Bugs/Bug_0131/Tests.lisp
@@ -4,14 +4,24 @@
 
  ("Bug 0131 : Prover goes into debugger"
   :sw "Option#P"
-  :output '(";;; Elaborating obligator at $TESTDIR/Option#P"
+  :output '(
+	    ";;; Elaborating obligator at $TESTDIR/Option#P"
 	    ";;; Elaborating spec at $TESTDIR/Option#O"
-	    (:optional ";;; Elaborating spec at $SPECWARE/Library/Base/WFO")
 	    (:optional ";;; Elaborating spec at $SPECWARE/Provers/ProofChecker/Libs")
 	    (:optional ";;; Elaborating spec at $SPECWARE/Library/General/FiniteStructures")
 	    (:optional ";;; Elaborating spec at $SPECWARE/Library/General/FiniteSequences")
+	    (:optional ";;; Elaborating spec at $SPECWARE/Library/General/Orders")
+	    (:optional ";;; Elaborating spec at $SPECWARE/Library/General/EndoRelations")
+	    (:optional ";;; Elaborating spec at $SPECWARE/Library/General/Relations")
+	    (:optional ";;; Elaborating spec at $SPECWARE/Library/General/Sets")
+	    (:optional ";;; Elaborating spec at $SPECWARE/Library/General/FiniteMaps")
+	    (:optional ";;; Elaborating spec at $SPECWARE/Library/General/FiniteSets")
+	    (:optional ";;; Elaborating spec at $SPECWARE/Library/General/Maps")
+	    (:optional ";;; Elaborating spec at $SPECWARE/Library/Structures/Data/Maps/SimpleAsAlist")
+	    (:optional ";;; Elaborating spec at $SPECWARE/Library/Structures/Data/Maps/Simple")
+	    (:optional ";;; Elaborating spec at $SPECWARE/Provers/ProofChecker/IntegerExt")
 	    (:optional ";;; Elaborating spec at $SPECWARE/Provers/ProofChecker/StateAndExceptionMonads")
-	    (:optional ";;; Elaborating spec at $SPECWARE/Library/Base/WFO"
+	    (:optional ";;; Elaborating spec at $SPECWARE/Library/Base/WFO")
 	    (:optional ";;; Elaborating proof-term at $TESTDIR/.")
 	    (:optional ";;; Elaborating spec at $SPECWARE/Library/ProverBase/Top")
 	    (:optional ";;; Elaborating spec at $SPECWARE/Library/ProverBase/Char")
@@ -25,11 +35,12 @@
 	    (:optional ";;; Elaborating spec at $SPECWARE/Library/ProverBase/System")
 	    (:optional ";;; Elaborating spec at $SPECWARE/Library/Base/ProverRewrite")
 	    (:optional ";; ensure-directories-exist: creating $TESTDIR/Snark/..log")
-	    (:optional "creating directory: $TESTDIR/Snark/")
 	    (:optional ";; Directory $TESTDIR/Snark/ does not exist, will create.")
-	    (:optional ";;; Elaborating spec at $SPECWARE/Library/Base/ProverBase")
 	    (:optional "    Expanded spec file: $TESTDIR/Snark/..sw")
 	    (:optional "    Snark Log file: $TESTDIR/Snark/..log")
-	    "Conjecture PFunctions.o_Obligation_exhaustive is Proved! using Snark in * seconds."
-	    "")))
+	    "Conjecture PFunctions.o_Obligation_exhaustive is Proved! *"
+	    (:optional "")
+	    (:optional "")
+	    ))
+
  )
