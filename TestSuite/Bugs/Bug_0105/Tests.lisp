394368056117b06827d5f0d29eebc58d1ecd2ada
Jim McDonald <mcdonald@kestrel.edu>
2006-12-04 21:37:05 +0000

    track new printer
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@4169 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- TestSuite/Bugs/Bug_0105/Tests.lisp
+++ TestSuite/Bugs/Bug_0105/Tests.lisp
@@ -6,14 +6,18 @@
   :show   "QuantifiedAxiom#A"
   :output '(";;; Elaborating spec at $TESTDIR/QuantifiedAxiom#A"
 	    ""
-	    "spec  "
+	    "spec"
+	    (:optional " ")
 	    " op  f infixl 22 : [a] List(a) * a -> Integer"
-	    " "
+	    (:optional " ")
+	    " op i : Nat"
 	    " def i = 123"
+	    (:optional " ")
 	    " axiom A is [i] f 3 = 0"
 	    "endspec"
-	    ""
-	    ""))
+	    (:optional " ")
+	    (:optional " ")))
+
 
  ("Bug 0105 B: The new-style type quantifications in claim definitions are ambiguous"
   :show   "QuantifiedAxiom#B"
@@ -21,20 +25,24 @@
 	    "Errors in $TESTDIR/QuantifiedAxiom.sw"
 	    "13.18-13.18	: Could not match type constraint"
 	    "                   3 of type Nat"
-	    "          with expected type List(mtv%metafy%5) * mtv%metafy%5"
-	    ""))
+	    "          with expected type List(mtv%metafy%*) * mtv%metafy%*"
+	    (:optional " ")))
+
 
  ("Bug 0105 C: The new-style type quantifications in claim definitions are ambiguous"
   :show   "QuantifiedAxiom#C"
   :output '(";;; Elaborating spec at $TESTDIR/QuantifiedAxiom#C"
-	    ""
-	    "spec  "
+	    (:optional " ")
+	    "spec"
+	    (:optional " ")
 	    " op  f infixl 22 : [a] a -> Integer"
-	    " "
+	    (:optional " ")
+	    " op i : Nat"
 	    " def i = 123"
+	    (:optional " ")
 	    " axiom A is [i] f(3) = 0"
 	    "endspec"
-	    ""
-	    ""))
+	    (:optional " ")
+	    (:optional " ")))
 
  )
