f5ef05dcfdd25729d5fb1be363239dc48cf10362
coredev <coredev@kestrel.edu>
2007-01-12 21:23:16 +0000

    get slime fasls from common location
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@4357 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- Library/IO/Emacs/load-slime.lisp
+++ Library/IO/Emacs/load-slime.lisp
@@ -1,3 +1,15 @@
+
+;; per instructions in swank-loader.lisp
+(cl:defpackage :swank-loader
+	       (:use :cl)
+	       (:export :load-swank 
+			:*source-directory*
+			:*fasl-directory*))
+
+(defparameter swank-loader::*fasl-directory* 
+  (format nil "~a/Library/IO/Emacs/slime/" 
+	  (sys:getenv "SPECWARE4")))
+
 (load (format nil "~a/Library/IO/Emacs/slime/swank-loader.lisp"
 	      (sys:getenv "SPECWARE4")))
 
