6b97a9a53eb3b8159b83b29b014d4686b34ebd9b
Jim McDonald <mcdonald@kestrel.edu>
2007-01-13 17:14:06 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@4363 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- Release/Windows/Allegro/SpecwareShell.cmd
+++ Release/Windows/Allegro/SpecwareShell.cmd
@@ -1,26 +1,10 @@
 @echo off
 
-rem Run Specware directly as a standalone application built using ACL
-rem To run this, the user does not need an ACL license,  but
-rem must meet criteria set forth in Kestrel's agreeement with Franz.
 
-rem A hack to try to ensure we connect to the proper directory,
-rem so the call to CheckSpecwareVars will succeed.
-rem The proper alternative is probably to parse %CMDCMDLINE%,
-rem but that is dreadful to contemplate.
-
-if exist Specware-4-2 ( cd Specware-4-2 )
+call check-and-set-environment
 
 
-call Find_SPECWARE4
-call Update_Path
-call Update_SWPATH
-call Find_Specware_App_ACL
+rem  Start Specware shell within Lisp shell:
 
-start "ignore" "%LISP_EXECUTABLE%"  +t "Specware Shell" +s "%SPECWARE4%\StartSpecwareShell.lisp" -I "%LISP_HEAP_IMAGE%" -e "(setq emacs::*use-emacs-interface?* nil)"
+start "ignore" "%LISP_EXECUTABLE%" +t "Specware Shell" +s "%SPECWARE4%\Applications\Specware\Handwritten\Lisp\StartShell.lisp" -I "%LISP_HEAP_IMAGE%" -e "(setq emacs::*use-emacs-interface?* nil)"
 
-rem  The following pause is just to keep the window that this 
-rem  script runs in from disappearing while debugging this script.
-rem  You can comment it in or out as you please.
-
-rem pause
---
+++ Release/Windows/Allegro/start-in-xemacs-xeli.cmd
@@ -0,0 +1,14 @@
+@echo off
+
+if "%1"=="/wait" (
+ set MAYBE_WAIT=/wait
+ shift
+) else (
+ set MAYBE_WAIT=
+)
+
+call check-and-set-environment
+
+rem  Start XEmacs loading various files and performing the action given as argument (i.e. %1):
+
+start "ignore" %MAYBE_WAIT% "%XEMACS%\i586-pc-win32\xemacs.exe" -l "%ALLEGRO%/xeli/fi-site-init" -l "%SPECWARE4%/Library/IO/Emacs/load" -f "%1"
