65f2500d5e0ca77da2293616d2cb506f7fd5858b
Stephen Westfold <westfold@kestrel.edu>
2007-02-01 22:47:13 +0000

    Add tuple utilities.
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@4417 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- Library/Isa/Base.thy
+++ Library/Isa/Base.thy
@@ -1,5 +1,21 @@
 theory Base
 imports Empty String Compare System Hilbert_Choice
 begin
+consts 
+  second  :: "'a * 'b * 'c \<Rightarrow> 'b"
+  thirdl  :: "'a * 'b * 'c \<Rightarrow> 'c"
+  third   :: "'a * 'b * 'c * 'd \<Rightarrow> 'c"
+  fourthl :: "'a * 'b * 'c * 'd \<Rightarrow> 'd"
+  fourth  :: "'a * 'b * 'c * 'd * 'e \<Rightarrow> 'd"
+  fifthl  :: "'a * 'b * 'c * 'd * 'e \<Rightarrow> 'e"
+  fifth   :: "'a * 'b * 'c * 'd * 'e * 'f \<Rightarrow> 'e"
 
+defs
+  second_def  [simp]: "second x \<equiv> fst(snd x)"
+  thirdl_def  [simp]: "thirdl x \<equiv> snd(snd x)"
+  third_def   [simp]: "third x \<equiv> fst(snd(snd x))"
+  fourthl_def [simp]: "fourthl x \<equiv> snd(snd(snd x))"
+  fourth_def  [simp]: "fourth x \<equiv> fst(snd(snd(snd x)))"
+  fifthl_def  [simp]: "fifthl x \<equiv> snd(snd(snd(snd x)))"
+  fifth_def   [simp]: "fifth x \<equiv> fst(snd(snd(snd(snd x))))"
 end
