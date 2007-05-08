8caad880597a88f6b4a54444e1c0d39eaf54000c
Stephen Westfold <westfold@kestrel.edu>
2007-05-08 01:17:43 +0000

    Add extra theorems
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@4565 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- Library/Isa/SW_List.thy
+++ Library/Isa/SW_List.thy
@@ -52,8 +52,11 @@ theorem List__length_nthTail_Obligation_subsort:
   "\<lbrakk>n \<le> length l\<rbrakk> \<Longrightarrow> 0 \<le> int (length l) - int n"
   apply(auto)
   done
-axioms List__length_nthTail [simp]: 
+theorem List__length_nthTail [simp]: 
   "\<lbrakk>n \<le> length l\<rbrakk> \<Longrightarrow> length (List__nthTail(l,n)) = length l - n"
+    apply(induct_tac l n rule: List__nthTail.induct)
+    apply(auto)
+  done
 theorem List__last_Obligation_subsort: 
   "\<lbrakk>\<not> (tl__v = [])\<rbrakk> \<Longrightarrow> length tl__v > 0"
   apply(auto)
@@ -62,32 +65,80 @@ theorem List__butLast_Obligation_subsort:
   "\<lbrakk>\<not> (tl__v = [])\<rbrakk> \<Longrightarrow> length tl__v > 0"
   apply(auto)
   done
-consts List__sublist__removeFirstElems :: "'a list \<times> nat \<Rightarrow> 'a list"
-theorem List__sublist__removeFirstElems_Obligation_subsort0: 
-  "\<lbrakk>\<not> (i = 0)\<rbrakk> \<Longrightarrow> 0 \<le> int i - 1"
+consts List__removeFirstElems :: "'a list \<times> nat \<Rightarrow> 'a list"
+theorem List__removeFirstElems_Obligation_subsort: 
+  "\<lbrakk>(i::nat) \<le> length l; \<not> (i = 0)\<rbrakk> \<Longrightarrow> \<not> (null l)"
+    apply(auto simp add: null_empty)
+  done
+theorem List__removeFirstElems_Obligation_subsort0: 
+  "\<lbrakk>i \<le> length (l::'a list); \<not> (i = 0)\<rbrakk> \<Longrightarrow> 0 \<le> int i - 1"
   apply(auto)
   done
-recdef List__sublist__removeFirstElems "measure (\<lambda>(l,i). i)"
-  "List__sublist__removeFirstElems(l,0) = l"
-  "List__sublist__removeFirstElems(l,Suc i) 
-     = List__sublist__removeFirstElems(tl l,i)"
+theorem List__removeFirstElems_Obligation_subsort1: 
+  "\<lbrakk>i \<le> length l; \<not> (i = 0)\<rbrakk> \<Longrightarrow> 
+   int i - 1 \<le> int (length (tl l))"
+  apply(auto)
+  done
+recdef List__removeFirstElems "measure (\<lambda>(l,i). i)"
+  "List__removeFirstElems(l,0) = l"
+  "List__removeFirstElems(l,Suc i) = List__removeFirstElems(tl l,i)"
+theorem List__length_removeFirstElems_Obligation_subsort: 
+  "\<lbrakk>i \<le> length l\<rbrakk> \<Longrightarrow> 0 \<le> int (length l) - int i"
+  apply(auto)
+  done
+theorem List__length_removeFirstElems [simp]: 
+  "\<lbrakk>i \<le> length l\<rbrakk> \<Longrightarrow> 
+   length (List__removeFirstElems(l,i)) = length l - i"
+    apply(induct_tac l i rule: List__removeFirstElems.induct)
+    apply(auto)
+  done
 consts List__sublist__collectFirstElems :: "'a list \<times> nat \<Rightarrow> 'a list"
+theorem List__sublist__collectFirstElems_Obligation_subsort: 
+  "\<lbrakk>(i::nat) \<le> length l; \<not> (i = 0)\<rbrakk> \<Longrightarrow> \<not> (null l)"
+    apply(auto simp add: null_empty)
+  done
 theorem List__sublist__collectFirstElems_Obligation_subsort0: 
-  "\<lbrakk>\<not> (null (l::'a list)); \<not> (null l); \<not> (i = 0)\<rbrakk> \<Longrightarrow> 0 \<le> int i - 1"
+  "\<lbrakk>(i::nat) \<le> length l; \<not> (i = 0)\<rbrakk> \<Longrightarrow> \<not> (null l)"
+    apply(auto simp add: null_empty)
+  done
+theorem List__sublist__collectFirstElems_Obligation_subsort1: 
+  "\<lbrakk>i \<le> length (l::'a list); \<not> (i = 0)\<rbrakk> \<Longrightarrow> 0 \<le> int i - 1"
+  apply(auto)
+  done
+theorem List__sublist__collectFirstElems_Obligation_subsort2: 
+  "\<lbrakk>i \<le> length l; \<not> (i = 0)\<rbrakk> \<Longrightarrow> 
+   int i - 1 \<le> int (length (tl l))"
   apply(auto)
   done
 recdef List__sublist__collectFirstElems "measure (\<lambda>(l,i). i)"
   "List__sublist__collectFirstElems(l,0) = []"
   "List__sublist__collectFirstElems(l,Suc i) 
      = Cons (hd l) (List__sublist__collectFirstElems(tl l,i))"
+theorem List__sublist_Obligation_subsort: 
+  "\<lbrakk>(i::nat) \<le> (j::nat); j \<le> length l\<rbrakk> \<Longrightarrow> i \<le> length l"
+  apply(auto)
+  done
 theorem List__sublist_Obligation_subsort0: 
   "\<lbrakk>i \<le> j; j \<le> length l\<rbrakk> \<Longrightarrow> 0 \<le> int j - int i"
   apply(auto)
   done
+theorem List__sublist_Obligation_subsort1: 
+  "\<lbrakk>i \<le> j; j \<le> length l\<rbrakk> \<Longrightarrow> 
+   int j - int i \<le> int (length (List__removeFirstElems(l,i)))"
+  apply(auto)
+  done
 recdef List__sublist "{}"
   "List__sublist(l,i,j)
-     = List__sublist__collectFirstElems(List__sublist__removeFirstElems(l,i),
-                                        j - i)"
+     = List__sublist__collectFirstElems(List__removeFirstElems(l,i),j - i)"
+theorem List__sublist_whole_Obligation_subsort: 
+  "\<lbrakk>(j::nat) = length l\<rbrakk> \<Longrightarrow> 0 \<le> j \<and> j \<le> length l"
+  apply(auto)
+  done
+theorem List__sublist_whole [simp]: 
+  "List__sublist(l,0,length l) = l"
+    apply(induct_tac l)
+    apply(auto)
+  done
 primrec 
   "List__foldl f base [] = base"
   "List__foldl f base (Cons hd_v tl_v) 
@@ -146,16 +197,34 @@ consts List__locationOf__checkPrefix :: "'a list \<times> 'a list \<Rightarrow> 
 consts List__locationOf__locationOfNonEmpty :: "'a list \<times> 'a list \<times> nat \<Rightarrow> 
                                                 (nat \<times> 'a list) option"
 theorem List__locationOf__locationOfNonEmpty_Obligation_subsort: 
-  "\<lbrakk>(subl::'a list) = Cons (subhd::'a) subtl; 
+  "\<lbrakk>Cons (subhd::'a) subtl \<noteq> []; 
     (supl::'a list) = Cons subhd suptl; 
     List__locationOf__checkPrefix(subtl,suptl) = None\<rbrakk> \<Longrightarrow> 
    0 \<le> int ((pos::nat) + 1)"
   apply(auto)
   done
 theorem List__locationOf__locationOfNonEmpty_Obligation_subsort0: 
-  "\<lbrakk>(subl::'a list) = Cons (subhd::'a) (subtl::'a list); 
+  "\<lbrakk>Cons (subhd::'a) (subtl::'a list) \<noteq> []; 
     (supl::'a list) = Cons (suphd::'a) (suptl::'a list); 
     \<not> (subhd = suphd)\<rbrakk> \<Longrightarrow> 0 \<le> int ((pos::nat) + 1)"
+  apply(auto)
+  done
+recdef List__locationOf__locationOfNonEmpty "measure (\<lambda>(subl,supl,pos). length supl)"
+  "List__locationOf__locationOfNonEmpty(Cons subhd subtl,[],pos) 
+     = None"
+  "List__locationOf__locationOfNonEmpty(Cons subhd subtl,
+                                        Cons suphd suptl,pos) 
+     = (if subhd = suphd then 
+          case List__locationOf__checkPrefix(subtl,suptl)
+           of None \<Rightarrow> 
+              List__locationOf__locationOfNonEmpty(Cons subhd subtl,suptl,
+                                                   pos + 1)
+            | Some suplrest \<Rightarrow> Some(pos,suplrest)
+        else 
+          List__locationOf__locationOfNonEmpty(Cons subhd subtl,suptl,
+                                               pos + 1))"
+theorem List__locationOf_Obligation_subsort: 
+  "\<lbrakk>\<not> ((subl::'a list) = [])\<rbrakk> \<Longrightarrow> subl \<noteq> []"
   apply(auto)
   done
 recdef List__locationOf "{}"
