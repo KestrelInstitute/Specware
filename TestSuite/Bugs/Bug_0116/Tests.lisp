c3b339e5f10741bf65b9050f13f718976ac5d6d4
Jim McDonald <mcdonald@kestrel.edu>
2007-05-09 00:59:04 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@4569 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- TestSuite/Bugs/Bug_0116/Tests.lisp
+++ TestSuite/Bugs/Bug_0116/Tests.lisp
@@ -4,8 +4,20 @@
  ("Bug 0116 : Error in removeCurrying"
   :sw  "RemCurr#P"
   :output    '(
+	       (:optional "")
 	       ";;; Elaborating spec at $TESTDIR/RemCurr#S"
 	       ";;; Elaborating proof-term at $TESTDIR/RemCurr#P"
+	       (:optional ";;; Elaborating spec at $SPECWARE/Library/ProverBase/Top")
+	       (:optional ";;; Elaborating spec at $SPECWARE/Library/ProverBase/Char")
+	       (:optional ";;; Elaborating spec at $SPECWARE/Library/ProverBase/Compare")
+	       (:optional ";;; Elaborating spec at $SPECWARE/Library/ProverBase/Functions")
+	       (:optional ";;; Elaborating spec at $SPECWARE/Library/ProverBase/Integer")
+	       (:optional ";;; Elaborating spec at $SPECWARE/Library/ProverBase/List")
+	       (:optional ";;; Elaborating spec at $SPECWARE/Library/ProverBase/Nat")
+	       (:optional ";;; Elaborating spec at $SPECWARE/Library/ProverBase/Option")
+	       (:optional ";;; Elaborating spec at $SPECWARE/Library/ProverBase/String")
+	       (:optional ";;; Elaborating spec at $SPECWARE/Library/ProverBase/System")
+	       (:optional ";;; Elaborating spec at $SPECWARE/Library/Base/ProverRewrite")
 	       (:optional ";; ensure-directories-exist: creating $TESTDIR/Snark/RemCurr/P.log")
 	       (:optional "creating directory: $TESTDIR/Snark/")
 	       (:optional ";; Directory $TESTDIR/Snark/ does not exist, will create.")
@@ -14,6 +26,8 @@
 	       "    Expanded spec file: $TESTDIR/Snark/RemCurr/P.sw"
 	       "    Snark Log file: $TESTDIR/Snark/RemCurr/P.log"
 	       "P: Conjecture TRUE in S is Proved! using simple inequality reasoning."
-	       ""))
+	       (:optional "")
+	       (:optional "")
+	       ))
 
  )
