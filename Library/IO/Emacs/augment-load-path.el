88b63307d803b6c6d70c0733d9be5f9aa76e6446
Jim McDonald <mcdonald@kestrel.edu>
2007-08-16 23:53:30 +0000

    Useful path locations
    My best guess is that these were built in to previous xemacs,
    or were present in some site-init file.
    
    
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@4751 7b97033b-253e-4a5c-9b07-e25f9089a9fd

---
+++ Library/IO/Emacs/augment-load-path.el
@@ -0,0 +1,17 @@
+;;; cvs and slime use things from these directories
+
+(setq load-path
+      (append load-path
+	      '("/usr/share/xemacs/xemacs-packages/lisp/dired/" 
+		"/usr/share/xemacs/xemacs-packages/lisp/xemacs-base/"
+		"/usr/share/xemacs/xemacs-packages/lisp/elib/"
+		"/usr/share/xemacs/xemacs-packages/lisp/pcl-cvs/"
+		"/usr/share/xemacs/xemacs-packages/lisp/xemacs-devel/"
+		"/usr/share/xemacs/xemacs-packages/lisp/edebug/"
+		"/usr/share/xemacs/xemacs-packages/lisp/edit-utils/"
+		"/usr/share/xemacs/xemacs-packages/lisp/mail-lib/"
+		"/usr/share/xemacs/xemacs-packages/lisp/c-support/"
+		"/usr/share/xemacs/xemacs-packages/lisp/vc/"
+		"/usr/share/xemacs/xemacs-packages/lisp/fsf-compat/"
+		"/usr/share/xemacs/xemacs-packages/lisp/ilisp/extra/"
+		)))
