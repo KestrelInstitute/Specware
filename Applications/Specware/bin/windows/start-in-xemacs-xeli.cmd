173e4c1065ab458ea3db8297b67582ee1cb808cf
Jim McDonald <mcdonald@kestrel.edu>
2007-12-17 17:38:10 +0000

    parameterize location of xeli dir
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@4960 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- Applications/Specware/bin/windows/start-in-xemacs-xeli.cmd
+++ Applications/Specware/bin/windows/start-in-xemacs-xeli.cmd
@@ -11,4 +11,4 @@ call check-and-set-environment
 
 rem  Start XEmacs loading various files and performing the action given as argument (i.e. %1):
 
-start "ignore" %MAYBE_WAIT% "%XEMACS%\i586-pc-win32\xemacs.exe" -l "%ALLEGRO%/xeli/fi-site-init" -l "%SPECWARE4%/Library/IO/Emacs/load" -f "%1"
+start "ignore" %MAYBE_WAIT% "%XEMACS%\i586-pc-win32\xemacs.exe" -l "%XELI%/fi-site-init" -l "%SPECWARE4%/Library/IO/Emacs/load" -f "%1"
