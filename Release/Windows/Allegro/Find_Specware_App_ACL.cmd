c09c0630fd5ccdf5740d61386354a93f74b703af
Stephen Westfold <westfold@kestrel.edu>
2008-02-01 02:13:17 +0000

    Had incorrectly changed image file to NONE which is wrong for allegro
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@4994 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- Release/Windows/Allegro/Find_Specware_App_ACL.cmd
+++ Release/Windows/Allegro/Find_Specware_App_ACL.cmd
@@ -19,7 +19,7 @@ rem named "specware".
 
 set "LISP_DIRECTORY=%SPECWARE4%/"
 set "LISP_EXECUTABLE=%SPECWARE4%/Specware4.exe"
-set "LISP_HEAP_IMAGE=NONE"
+set "LISP_HEAP_IMAGE=%SPECWARE4%/Specware4.dxl"
 
 if not exist "%LISP_EXECUTABLE%" (
   echo Cannot find Specware4 executable: %LISP_EXECUTABLE%
