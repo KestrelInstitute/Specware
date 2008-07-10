be34e4b8aca4f6c1ac7e3d6aef5685ea3db2214e
Stephen Westfold <westfold@kestrel.edu>
2008-07-10 23:21:49 +0000

    Move basic stuff from Base to Empty
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@5086 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- Library/Isa/Base.thy
+++ Library/Isa/Base.thy
@@ -1,26 +1,5 @@
 theory Base
 imports Empty String Compare System
 begin
-consts 
-  second  :: "'a * 'b * 'c \<Rightarrow> 'b"
-  thirdl  :: "'a * 'b * 'c \<Rightarrow> 'c"
-  third   :: "'a * 'b * 'c * 'd \<Rightarrow> 'c"
-  fourthl :: "'a * 'b * 'c * 'd \<Rightarrow> 'd"
-  fourth  :: "'a * 'b * 'c * 'd * 'e \<Rightarrow> 'd"
-  fifthl  :: "'a * 'b * 'c * 'd * 'e \<Rightarrow> 'e"
-  fifth   :: "'a * 'b * 'c * 'd * 'e * 'f \<Rightarrow> 'e"
-
-defs
-  second_def  [simp]: "second x \<equiv> fst(snd x)"
-  thirdl_def  [simp]: "thirdl x \<equiv> snd(snd x)"
-  third_def   [simp]: "third x \<equiv> fst(snd(snd x))"
-  fourthl_def [simp]: "fourthl x \<equiv> snd(snd(snd x))"
-  fourth_def  [simp]: "fourth x \<equiv> fst(snd(snd(snd x)))"
-  fifthl_def  [simp]: "fifthl x \<equiv> snd(snd(snd(snd x)))"
-  fifth_def   [simp]: "fifth x \<equiv> fst(snd(snd(snd(snd x))))"
-
-consts
-  Ex1_DP           :: "('a => bool) => (('a => 'b) => bool) => bool"
-defs Ex1_DP_def [simp]: "Ex1_DP (DP)(P)    == ? f. (P(f)) & (! g. P(g) --> (! x. DP x --> f x = g x))"
-  
+ 
 end
