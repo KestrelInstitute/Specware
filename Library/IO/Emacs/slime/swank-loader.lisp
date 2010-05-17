e31b90f7f398deb383d5465da7cbc799bfd4bb8d
Jim McDonald <mcdonald@kestrel.edu>
2010-05-17 18:02:22 +0000

    avoid referencing non-existant symbols
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@6119 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- Library/IO/Emacs/slime/swank-loader.lisp
+++ Library/IO/Emacs/slime/swank-loader.lisp
@@ -253,11 +253,15 @@ If LOAD is true, load the fasl file."
 (defun setup ()
   (load-site-init-file *source-directory*)
   (load-user-init-file)
-  (when (#-clisp probe-file
-         #+clisp ext:probe-directory        
-         (contrib-dir *source-directory*))
-    (eval `(pushnew 'compile-contribs ,(q "swank::*after-init-hook*"))))
-  (funcall (q "swank::init")))
+  (let ((x (find-symbol "*AFTER-INIT-HOOK*" "SWANK"))
+        (y (find-symbol "INIT" "SWANK")))
+    (when (#-clisp probe-file
+                   #+clisp ext:probe-directory        
+                   (contrib-dir *source-directory*))
+      (when (symbol-value x)
+        (eval `(pushnew 'compile-contribs ,(q "swank::*after-init-hook*")))))
+    (when(symbol-function y)
+      (funcall (q "swank::init")))))
 
 (defun init (&key delete reload load-contribs (setup t))
   "Load SWANK and initialize some global variables.
@@ -272,10 +276,13 @@ global variabes in SWANK."
          (load-swank))
         (t 
          (warn "Not reloading SWANK.  Package already exists.")))
+  (let ((x (find-symbol "*AFTER-INIT-HOOK*" "SWANK")))
   (when load-contribs
     (compile-contribs :load t))
   (when setup
-    (setup)))
+    (setup))))
+
+         
 
 (defun dump-image (filename)
   (init :setup nil)
