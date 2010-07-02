2f6913507403b0cdd4a36857006d58f663b8c46c
Stephen Westfold <westfold@kestrel.edu>
2010-07-02 06:31:58 +0000

    Use (getenv  "SPECWARE4") to ensure Specware dir is fresh
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@6222 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- Release/Generic/CheckLicense.lisp
+++ Release/Generic/CheckLicense.lisp
@@ -8,14 +8,14 @@
                                     cl-user::*Specware-Minor-Version*
                                     cl-user::*Specware-Patch-Level*))
 
-(defun display-license-and-accept ()
-  (let ((license-text-file (format nil "~a/~a" *Specware4* *license-text-file*)))
+(defun display-license-and-accept (specware-dir)
+  (let ((license-text-file (format nil "~a/~a" specware-dir *license-text-file*)))
     (if Emacs::*use-emacs-interface?*
         (Emacs::eval-in-emacs (format nil "(display-license-and-accept ~s)" license-text-file))
         (progn
           (format t "Opening ~a~%" license-text-file)
           (and (ignore-errors
-                 (with-open-file (s (format nil "~a/~a" *Specware4* *license-text-file*)
+                 (with-open-file (s (format nil "~a/~a" specware-dir *license-text-file*)
                                     :direction :input)
                    (loop for line = (read-line s  nil :done)
                          until (eq line :done)
@@ -32,8 +32,8 @@
     (let* ((home-dir (ensure-final-slash (to-cygwin-name (cl-user::home-dir))))
            (home-file-name (format nil "~a~a" home-dir *license-file*))
            (home-file (probe-file home-file-name))
-           (specware-file-name (format nil "~a~a" *Specware4*
-                                       *license-file*))
+           (specware-dir (Specware::getenv "SPECWARE4"))
+           (specware-file-name (format nil "~a/~a" specware-dir *license-file*))
            (license-file (or home-file (probe-file specware-file-name)))
            (license-val (ignore-errors
                           (with-open-file (s license-file :direction :input)
@@ -46,7 +46,7 @@
                         (car (List-Spec::compare-1-1 #'Integer-Spec::compare
                                                      (cons license-val *version-sexpr*))))))
           (setq *license-accepted?* t)
-          (if (display-license-and-accept)
+          (if (display-license-and-accept specware-dir)
               (license-accepted)
             (if Emacs::*use-emacs-interface?*
                 (setq *license-accepted?* nil) ; Asynchronous
