fd8ba7463f9427fbf66dc19fe432110b8cd04b2c
Jim McDonald <mcdonald@kestrel.edu>
2011-09-20 02:48:54 +0000

    many minor tweaks
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@6967 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- Library/Clib/SWC_Common.h
+++ Library/Clib/SWC_Common.h
@@ -10,8 +10,6 @@
 
 #include "SWC_Memory.h"
 
-#define int_22 int
-
 /* 
  *  BUILT IN TYPES: 
  *
@@ -88,8 +86,8 @@
 #define FALSE 0
 #define TRUE 1
 
-char* Bool_show(int n) {
-  char *res = swc_malloc(sizeof(char)*6);
+char* Bool_show (int n) {
+  char* res = swc_malloc(sizeof(char)*6);
   if (n) {
     strcpy(res,"true");
   } else {
@@ -98,8 +96,8 @@ char* Bool_show(int n) {
   return res;
 }
 
-char* Bool_toString(int n) {
-  char *res = swc_malloc(sizeof(char)*6);
+char* Bool_toString (int n) {
+  char* res = swc_malloc(sizeof(char)*6);
   if (n) {
     strcpy(res,"true");
   } else {
@@ -135,11 +133,11 @@ typedef int Integer_Int0;
 /* "+", "-", "*", "div", "rem", "<=", "<", "~", ">", ">=", "**" */
 
 int Integer_isucc (int i) {
-  {return (i + 1);}
+  return (i + 1);
 }
 
 int Integer_ipred (int i) {
-  {return (i - 1);}
+  return (i - 1);
 }
 
 // char* Integer_show(int n) {
@@ -151,12 +149,12 @@ int Integer_ipred (int i) {
 //   return res;
 // }
 
-char* Integer_toString(int n) {
+char* Integer_toString (int n) {
   char buf[12];
   char *res;
-  sprintf(buf,"%d",n);
-  res = swc_malloc(strlen(buf)+1);
-  strcpy(res,buf);
+  sprintf (buf, "%d", n);
+  res = swc_malloc (strlen(buf) + 1);
+  strcpy (res, buf);
   return res;
 }
 
@@ -167,7 +165,7 @@ typedef uint16_t      u2;	// 16 bits (unsigned), see <stdint.h>
 typedef uint32_t      u4;	// 32 bits (unsigned)
 typedef uint64_t      u8;	// 64 bits (unsigned)
 
-#define Nat_show Integer_show
+#define Nat_show     Integer_show
 #define Nat_toString Integer_toString
 
 /*********************  LISTS  **************************/
@@ -179,7 +177,7 @@ typedef char Char_Char;
 #define Char_chr(_Int_)  ((char)_Int_)
 #define Char_ord(_Char_) ((uint32_t)_Char_)
 
-COMPARISON Char_compare(char c1, char c2) {
+COMPARISON Char_compare (char c1, char c2) {
   /* 
    *  TODO:  Would prefer to have the effect of the calls to SetConstructor
    *         built in at compile time, or done once during initialization.
@@ -202,7 +200,7 @@ COMPARISON Char_compare(char c1, char c2) {
 
 #define String_length strlen
 
-COMPARISON String_compare(char *s1, char *s2) {
+COMPARISON String_compare (char* s1, char* s2) {
   /* 
    *  TODO:  Would prefer to have the effect of the calls to SetConstructor
    *         built in at compile time, or done once during initialization.
@@ -222,16 +220,16 @@ COMPARISON String_compare(char *s1, char *s2) {
   }
 }
 
-char* String_append(char *s1, char *s2) {
-  char *res = swc_malloc(strlen(s1)+strlen(s2)+1);
-  strcpy(res,s1);
-  strcpy(res+strlen(s1),s2);
+char* String_append(char* s1, char* s2) {
+  char *res = swc_malloc (strlen(s1) + strlen(s2) + 1);
+  strcpy (res,            s1);
+  strcpy (res+strlen(s1), s2);
   return res;
 }
 
 #define String_PlusPlus String_append
-#define String_Caret String_append
-#define String_Less strcmp
+#define String_Caret    String_append
+#define String_Less     strcmp
 
 #define String_newline "\n"
 
@@ -240,11 +238,11 @@ char* String_append(char *s1, char *s2) {
 typedef double Float_Float;
 
 Float_Float Float_one_half = 0.5;
-Float_Float Float_one   = 1.0;
-Float_Float Float_two   = 2.0;
-Float_Float Float_three = 3.0;
-Float_Float Float_zero  = 0.0;
-Float_Float epsilon     = 3.0e-8;
+Float_Float Float_one      = 1.0;
+Float_Float Float_two      = 2.0;
+Float_Float Float_three    = 3.0;
+Float_Float Float_zero     = 0.0;
+Float_Float epsilon        = 3.0e-8;
 
 Float_Float Float_abs (Float_Float x) {
   return (fabs (x));
@@ -259,24 +257,17 @@ Float_Float Float_Slash (Float_Float x, Float_Float y) {
 }
 
 int Float_EqualEqual (Float_Float x, Float_Float y) {
-  if (x = y) {
+  if (x == y) {
     return 1;
   } else {
     return 0;
   }
 }
 
-/*
- * TODO:  ?? What was this doing here ??
- * Float_Float f (Float_Float x) {
- *  return ((34 * x * x * x) - (99.0 * x * x) + (1047 * x) + 12345.0);
- * }
- */
-
 /*********************  IO  *****************************/
 
-void System_writeLine(char *s) {
-  printf("%s\n",s);
+void System_writeLine (char *s) {
+  printf ("%s\n", s);
 }
 
 #define System_toScreen System_writeLine
@@ -290,13 +281,13 @@ int _error (char* s) {
   return 0;
 }
 
-int System_fail(char* msg) {
-  fprintf(stderr,"Error: %s\n",msg);
+int System_fail (char* msg) {
+  fprintf (stderr, "Error: %s\n", msg);
   exit(1);
 }
 
 int TranslationBuiltIn_mkFail (char* msg) {
-  fprintf(stderr,"Failure: %s\n",msg);
+  fprintf (stderr, "Failure: %s\n", msg);
   exit(1);
 }
 
@@ -315,7 +306,6 @@ int TranslationBuiltIn_mkFail (char* msg) {
 #define show      Integer_show
 #define toString  Integer_toString
 
-
 /*********************  ACCORD  *************************/
 
 typedef int Accord_ProcType ();
@@ -326,14 +316,18 @@ typedef int Accord_ProcType ();
 u4 MatchSuccess;
 u4 MatchValue;
 
-#define TranslationBuiltIn_block(form)     (form)
 /*
  * Note that failWith will always be within a cast back to the type of aa,
- * so any cast to int done by mkSuccess will be properly reversed back to 
+ * so any cast to u4 done by mkSuccess will be properly reversed back to 
  * the correct type if MatchValue is returned from failWith.
+ *
+ * TODO: check somewhere that u4 is sufficiently large to hold the value.
+ * TODO: maybe use u8 if value is large
  */
+
+#define TranslationBuiltIn_block(form)     (form)
 #define TranslationBuiltIn_failWith(aa,bb) ((aa), (MatchSuccess == TRUE) ? MatchValue : (bb))
-#define TranslationBuiltIn_mkSuccess(cc)   (MatchSuccess = TRUE, MatchValue = (int) cc)
+#define TranslationBuiltIn_mkSuccess(cc)   (MatchSuccess = TRUE, MatchValue = (u4) cc)
 #define TranslationBuiltIn_mkBreak         (MatchSuccess = FALSE)
 #define TranslationBuiltIn_mkFail(msg)     (MatchSuccess = FALSE)
 
