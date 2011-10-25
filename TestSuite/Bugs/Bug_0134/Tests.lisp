1a7b80c8591d91ba3f3ff032d05b5b118ad048ec
Jim McDonald <mcdonald@kestrel.edu>
2011-10-25 12:38:40 +0000

    cosmetic, printer has changed slightly
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@7052 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- TestSuite/Bugs/Bug_0134/Tests.lisp
+++ TestSuite/Bugs/Bug_0134/Tests.lisp
@@ -50,44 +50,45 @@
               "   fa(x : a, y : a, z : b) ")
              ("def foldable? (s : FSet(a), c : b, f : b * a -> b) : Bool"
               "  = fa(x : a, y : a, z : b) "))
-            (:alternatives
-             "    x in? s && y in? s => f(f(z, x), y) = f(f(z, y), x)"
-             "    x in? s && y in? s => f (f (z, x), y) = f (f (z, y), x)")
+            "    x in? s && y in? s => f (f (z, x), y) = f (f (z, y), x)"
             (:optional "")
 	    " op fold : [a, b] ((FSet(a) * b * (b * a -> b)) | foldable?) -> b"
             (:optional "")
 	    " conjecture fold_Obligation_subtype1 is [a, b] "
-            "   fa(fold : ((FSet(a) * b * (b * a -> b)) | foldable?) -> b, s : FSet(a), "
-            "      x : a, c_1 : b, f_1 : b * a -> b) "
-            "    (fa(c : b, f : b * a -> b) fold(empty, c, f) = c) "
-            "    && foldable?(s with x, c_1, f_1) => foldable?(s wout x, c_1, f_1)"
+            (:alternatives
+             ("   fa(fold : ((FSet(a) * b * (b * a -> b)) | foldable?) -> b, s : FSet(a), "
+              "      x : a, c_1 : b, f_1 : b * a -> b) "
+              "    (fa(c : b, f : b * a -> b) fold (empty, c, f) = c) "
+              "    && foldable?(s with x, c_1, f_1) => foldable?(s wout x, c_1, f_1)")
+             ("   fa(f_1 : b * a -> b, c_1 : b, s : FSet(a), x : a) "
+              "    foldable?(s with x, c_1, f_1) => foldable?(s wout x, c_1, f_1)"))
             (:optional "")
 	    " conjecture fold_Obligation_subtype0 is [a, b] "
 	    "    fa(c : b, f : b * a -> b) foldable?(empty, c, f)"
             (:optional "")
 	    " conjecture fold_Obligation_subtype is [a, b] "
 	    "    uniquelySatisfied?"
-	    "      ((fn fold -> "
-	    "           (fa(c : b, f : b * a -> b) fold(empty, c, f) = c) "
-	    "           && (fa(s : FSet(a), x : a, c_1 : b, f_1 : b * a -> b) "
-	    "                (foldable?(s with x, c_1, f_1) "
-	    "                => fold(s with x, c_1, f_1) "
-	    "                   = f_1(fold(s wout x, c_1, f_1), x)))))"
-            (:optional "")
             (:alternatives
-             (" def fold = "
-              "   The")
-             ("def fold : ((FSet(a) * b * (b * a -> b)) | foldable?) -> b"
-              "  = The"))
-            (:alternaticves
-             "     ((fn fold -> "
-             ("    ((fn fold : "
-              "         ((FSet(a) * b * (b * a -> b)) | "
-              "         foldable? : FSet(a) * b * (b * a -> b) -> Bool) -> b -> "))
-	    "          (fa(c : b, f : b * a -> b) fold(empty, c, f) = c) "
-	    "          && (fa(s : FSet(a), x : a, c : b, f : b * a -> b) "
-	    "               (foldable?(s with x, c, f) "
-	    "               => fold(s with x, c, f) = f(fold(s wout x, c, f), x)))))"
+             ("      ((fn fold -> "
+              "           (fa(c : b, f : b * a -> b) fold (empty, c, f) = c) "
+              "           && (fa(s : FSet(a), x : a, c_1 : b, f_1 : b * a -> b) "
+              "                (foldable?(s with x, c_1, f_1) "
+              "                => fold (s with x, c_1, f_1) "
+              "                   = f_1(fold (s wout x, c_1, f_1), x)))))")
+             ("     (fn (fold : ((FSet(a) * b * (b * a -> b)) | foldable?) -> b) -> "
+              "        (fa(c : b, f : b * a -> b) fold (empty, c, f) = c) "
+              "         && (fa(s : FSet(a), x : a, c_1 : b, f_1 : b * a -> b) "
+              "              (foldable?(s with x, c_1, f_1) "
+              "               => fold (s with x, c_1, f_1) "
+              "                   = f_1 (fold (s wout x, c_1, f_1), x)))) "))
+            (:optional "")
+            " def fold : ((FSet(a) * b * (b * a -> b)) | foldable?) -> b"
+            "   = The"
+            "       (fn (fold : ((FSet(a) * b * (b * a -> b)) | foldable?) -> b) -> "
+            "           (fa(c : b, f : b * a -> b) fold (empty, c, f) = c) "
+            "            && (fa(s : FSet(a), x : a, c : b, f : b * a -> b) "
+            "                 (foldable?(s with x, c, f) "
+            "                  => fold (s with x, c, f) = f (fold (s wout x, c, f), x))))"
             (:optional "")
             (:alternatives "endspec" "end-spec")
             (:optional "")
