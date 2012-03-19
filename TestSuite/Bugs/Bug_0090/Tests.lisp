88ef97a56902dfccd343b8e819881a81d4b242bd
Jim McDonald <mcdonald@kestrel.edu>
2012-03-19 23:39:52 +0000

    track recent changes to pprinter
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@7307 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- TestSuite/Bugs/Bug_0090/Tests.lisp
+++ TestSuite/Bugs/Bug_0090/Tests.lisp
@@ -11,21 +11,17 @@
 	    "spec"
 	    (:optional " import /Library/Base/WFO")
 	    (:optional "")
-	    " op  f : [a] List(a) -> Nat"
+            "op [a] f: List(a) -> Nat"
 	    (:optional "")
-	    " conjecture f_Obligation_subtype is [a] "
-	    (:optional "")
+	    "conjecture f_Obligation_subtype is [a]"
             (:alternatives
-             "    fa(l : List(a)) ~(l = []) => posNat?(length l)"
-             ("  fa(l : List(a)) "
-              "   ~(l = []) && length l >= 0 => posNat?(length l)"))
+             "fa(l: List(a)) ~(l = []) => posNat?(length l)"
+             "fa(l : List(a)) ~(l = []) => posNat?(length l)"
+             "fa(l: List(a)) ~(l = []) && length l >= 0 => posNat?(length l)")
 	    (:optional "")
-            (:alternatives
-             " def f l = (case l"
-             (" def f (l : List(a)) : Nat"
-              "   = case l"))
-            "      of [] -> 0"
-            "      | _ -> (100 div length l)"
+            "def [a] f(l: List(a)): Nat = case l"
+            "of [] -> 0"
+            "| _ -> (100 div length l)"
 	    (:optional "")
             (:alternatives "endspec" "end-spec")
 	    (:optional "")
