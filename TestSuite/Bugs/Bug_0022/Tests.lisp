8058b0ac6bbf66f70b0d058a9e20df4a80286d56
Jim McDonald <mcdonald@kestrel.edu>
2012-03-31 01:23:43 +0000

    Fixed some test scripts to avoid curious sudden mangling of expected output strings
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@7374 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- TestSuite/Bugs/Bug_0022/Tests.lisp
+++ TestSuite/Bugs/Bug_0022/Tests.lisp
@@ -24,47 +24,62 @@
 
  ("Bug 0022 : Redefined ops processed without giving error message [DefOp]"
   :sw "RedefinedOp#DefOp"
-  :output ";;; Elaborating spec at $TESTDIR/RedefinedOp#DefOp
-Error in specification: Operator f has been redeclared
- from 3
-   to <anyterm>: Nat
- found in $TESTDIR/RedefinedOp.sw
-8.1-8.10")
+  :output '((:optional "")
+            ";;; Elaborating spec at $TESTDIR/RedefinedOp#DefOp"
+            "Error in specification: Operator f has been redeclared"
+            " from 3"
+            "   to <anyterm>: Nat"
+            " found in $TESTDIR/RedefinedOp.sw"
+            "8.1-8.10"
+            (:optional "")
+            ))
 
  ("Bug 0022 : Redefined ops processed without giving error message [DefDef]"
   :sw "RedefinedOp#DefDef"
-  :output ";;; Elaborating spec at $TESTDIR/RedefinedOp#DefDef
-Error in specification: Operator f has been redefined
- from 3
-   to 3
- found in $TESTDIR/RedefinedOp.sw
-13.1-13.9")
+  :output '((:optional "")
+            ";;; Elaborating spec at $TESTDIR/RedefinedOp#DefDef"
+            "Error in specification: Operator f has been redefined"
+            " from 3"
+            "   to 3"
+            " found in $TESTDIR/RedefinedOp.sw"
+            "13.1-13.9"
+            (:optional "")
+            ))
 
  ("Bug 0022 : Redefined ops processed without giving error message [OpOp]"
   :sw "RedefinedOp#OpOp"
-  :output ";;; Elaborating spec at $TESTDIR/RedefinedOp#OpOp
-Error in specification: Operator f has been redeclared
- from Nat
-   to Nat
- found in $TESTDIR/RedefinedOp.sw
-18.1-18.10")
+  :output '((:optional "")
+            ";;; Elaborating spec at $TESTDIR/RedefinedOp#OpOp"
+            "Error in specification: Operator f has been redeclared"
+            " from Nat"
+            "   to Nat"
+            " found in $TESTDIR/RedefinedOp.sw"
+            "18.1-18.10"
+            (:optional "")
+            ))
 
  ("Bug 0022 : Redefined ops processed without giving error message [OpDefOp]"
   :sw "RedefinedOp#OpDefOp"
-  :output ";;; Elaborating spec at $TESTDIR/RedefinedOp#OpDefOp
-Error in specification: Operator f has been redeclared
- from 3: Nat
-   to <anyterm>: Nat
- found in $TESTDIR/RedefinedOp.sw
-24.1-24.10")
+  :output '((:optional "")
+            ";;; Elaborating spec at $TESTDIR/RedefinedOp#OpDefOp"
+            "Error in specification: Operator f has been redeclared"
+            " from 3: Nat"
+            "   to <anyterm>: Nat"
+            " found in $TESTDIR/RedefinedOp.sw"
+            "24.1-24.10"
+            (:optional "")
+            ))
 
  ("Bug 0022 : Redefined ops processed without giving error message [OpDefDef]"
   :sw "RedefinedOp#OpDefDef"
-  :output ";;; Elaborating spec at $TESTDIR/RedefinedOp#OpDefDef
-Error in specification: Operator f has been redefined
- from 3: Nat
-   to 3
- found in $TESTDIR/RedefinedOp.sw
-30.1-30.9")
+  :output '((:optional "")
+            ";;; Elaborating spec at $TESTDIR/RedefinedOp#OpDefDef"
+            "Error in specification: Operator f has been redefined"
+            " from 3: Nat"
+            "   to 3"
+            " found in $TESTDIR/RedefinedOp.sw"
+            "30.1-30.9"
+            (:optional "")
+            ))
 
  )
