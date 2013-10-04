fe9f54d972ef3c17d6d0ca4385c39f7fa6a1610d
Eric Smith <ewsmith@kestrel.edu>
2013-10-04 21:38:13 +0000

    Add Isabelle obligation generation (TODO: do the proofs).
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@8631 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- Examples/Tutorial/test.sh
+++ Examples/Tutorial/test.sh
@@ -31,3 +31,32 @@ run-proc.sh ${SPECWARE4} ${SPECWARE4}/Examples/Tutorial/MatchingRefinements#Find
 
 run-proc.sh ${SPECWARE4} ${SPECWARE4}/Examples/Tutorial/MatchingTest#Test
 run-proc.sh ${SPECWARE4} ${SPECWARE4}/Examples/Tutorial/MatchingTest#Data
+
+echo "  Removing existing Isabelle obligations:"
+clear-isabelle-dir.sh ${SPECWARE4}/Examples/Tutorial/Isa
+
+echo "  Generating Isabelle obligations:"
+
+run-gen-obligs.sh ${SPECWARE4} ${SPECWARE4}/Examples/Tutorial/MatchingSpecs#Symbols
+run-gen-obligs.sh ${SPECWARE4} ${SPECWARE4}/Examples/Tutorial/MatchingSpecs#Words
+run-gen-obligs.sh ${SPECWARE4} ${SPECWARE4}/Examples/Tutorial/MatchingSpecs#Messages
+run-gen-obligs.sh ${SPECWARE4} ${SPECWARE4}/Examples/Tutorial/MatchingSpecs#SymbolMatching
+run-gen-obligs.sh ${SPECWARE4} ${SPECWARE4}/Examples/Tutorial/MatchingSpecs#WordMatching
+run-gen-obligs.sh ${SPECWARE4} ${SPECWARE4}/Examples/Tutorial/MatchingSpecs#Matches
+run-gen-obligs.sh ${SPECWARE4} ${SPECWARE4}/Examples/Tutorial/MatchingSpecs#FindMatches
+
+run-gen-obligs.sh ${SPECWARE4} ${SPECWARE4}/Examples/Tutorial/MatchingRefinements#Symbols
+run-gen-obligs.sh ${SPECWARE4} ${SPECWARE4}/Examples/Tutorial/MatchingRefinements#Symbols_Ref
+run-gen-obligs.sh ${SPECWARE4} ${SPECWARE4}/Examples/Tutorial/MatchingRefinements#WordMatching0
+run-gen-obligs.sh ${SPECWARE4} ${SPECWARE4}/Examples/Tutorial/MatchingRefinements#WordMatching_Ref0
+run-gen-obligs.sh ${SPECWARE4} ${SPECWARE4}/Examples/Tutorial/MatchingRefinements#WordMatching
+run-gen-obligs.sh ${SPECWARE4} ${SPECWARE4}/Examples/Tutorial/MatchingRefinements#WordMatching_Ref
+run-gen-obligs.sh ${SPECWARE4} ${SPECWARE4}/Examples/Tutorial/MatchingRefinements#FindMatches0
+run-gen-obligs.sh ${SPECWARE4} ${SPECWARE4}/Examples/Tutorial/MatchingRefinements#FindMatches_Ref0
+run-gen-obligs.sh ${SPECWARE4} ${SPECWARE4}/Examples/Tutorial/MatchingRefinements#FindMatches
+run-gen-obligs.sh ${SPECWARE4} ${SPECWARE4}/Examples/Tutorial/MatchingRefinements#FindMatches_Ref
+
+run-gen-obligs.sh ${SPECWARE4} ${SPECWARE4}/Examples/Tutorial/MatchingTest#Test
+run-gen-obligs.sh ${SPECWARE4} ${SPECWARE4}/Examples/Tutorial/MatchingTest#Data
+
+# TODO: Add calls to actually do the proofs in Isabelle.
