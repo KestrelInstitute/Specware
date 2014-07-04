21ffb3e4479c049597ce48a49a789c69e0c72caf
Eric Smith <ewsmith@kestrel.edu>
2014-07-04 01:24:27 +0000

    Fix script to not require CDing into directory first.
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@9299 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- Examples/Misc/test.sh
+++ Examples/Misc/test.sh
@@ -8,14 +8,14 @@ fi
 SPECWARE4=$1
 ISABELLE_2013_2_ROOT=$2
 
-run-proc.sh ${SPECWARE4} TwoMorphisms#A
-run-proc.sh ${SPECWARE4} TwoMorphisms#B
-run-proc.sh ${SPECWARE4} TwoMorphisms#CheckRqmtsB
-run-proc.sh ${SPECWARE4} TwoMorphisms#C
-run-proc.sh ${SPECWARE4} TwoMorphisms#CheckRqmtsC
-run-gen-obligs.sh ${SPECWARE4} TwoMorphisms#A
-run-gen-obligs.sh ${SPECWARE4} TwoMorphisms#B
-run-gen-obligs.sh ${SPECWARE4} TwoMorphisms#CheckRqmtsB
-run-gen-obligs.sh ${SPECWARE4} TwoMorphisms#C
-run-gen-obligs.sh ${SPECWARE4} TwoMorphisms#CheckRqmtsC
-run-isabelle2013-2.sh Isa/TwoMorphisms_CheckRqmtsC.thy ${ISABELLE_2013_2_ROOT}
+run-proc.sh ${SPECWARE4} ${SPECWARE4}/Examples/Misc/TwoMorphisms#A
+run-proc.sh ${SPECWARE4} ${SPECWARE4}/Examples/Misc/TwoMorphisms#B
+run-proc.sh ${SPECWARE4} ${SPECWARE4}/Examples/Misc/TwoMorphisms#CheckRqmtsB
+run-proc.sh ${SPECWARE4} ${SPECWARE4}/Examples/Misc/TwoMorphisms#C
+run-proc.sh ${SPECWARE4} ${SPECWARE4}/Examples/Misc/TwoMorphisms#CheckRqmtsC
+run-gen-obligs.sh ${SPECWARE4} ${SPECWARE4}/Examples/Misc/TwoMorphisms#A
+run-gen-obligs.sh ${SPECWARE4} ${SPECWARE4}/Examples/Misc/TwoMorphisms#B
+run-gen-obligs.sh ${SPECWARE4} ${SPECWARE4}/Examples/Misc/TwoMorphisms#CheckRqmtsB
+run-gen-obligs.sh ${SPECWARE4} ${SPECWARE4}/Examples/Misc/TwoMorphisms#C
+run-gen-obligs.sh ${SPECWARE4} ${SPECWARE4}/Examples/Misc/TwoMorphisms#CheckRqmtsC
+run-isabelle2013-2.sh ${SPECWARE4}/Examples/Misc/Isa/TwoMorphisms_CheckRqmtsC.thy ${ISABELLE_2013_2_ROOT}
