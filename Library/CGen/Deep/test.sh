192fbece51ffe1e7ac72bd354c1b8f1b66676f75
Eric Smith <ewsmith@kestrel.edu>
2014-07-07 03:05:21 +0000

    Comment out test pending Westfold's improvement to not require a temp file for eval.
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@9303 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- Library/CGen/Deep/test.sh
+++ Library/CGen/Deep/test.sh
@@ -13,13 +13,15 @@ run-proc.sh ${SPECWARE4} ${SPECWARE4}/Library/CGen/Deep/CPrettyPrinter
 run-proc.sh ${SPECWARE4} ${SPECWARE4}/Library/CGen/Deep/CPrettyPrinter_Tests
 run-proc.sh ${SPECWARE4} ${SPECWARE4}/Library/CGen/Deep/All
 
-#Actually run the tests:
-LOG=${SPECWARE4}/Library/CGen/Deep/CPrettyPrinter_Tests.testlog
-run-specware-batch.sh ${SPECWARE4} ${LOG} <<EOF
-ctext ${SPECWARE4}/Library/CGen/Deep/CPrettyPrinter_Tests
-e run_test
-quit
-EOF
+# Currently causes an error in Bamboo due to dash in file name:
+# FIXME: Uncomment, once a temp file doesn't need to be generated to eval a value:
+# #Actually run the tests:
+# LOG=${SPECWARE4}/Library/CGen/Deep/CPrettyPrinter_Tests.testlog
+# run-specware-batch.sh ${SPECWARE4} ${LOG} <<EOF
+# ctext ${SPECWARE4}/Library/CGen/Deep/CPrettyPrinter_Tests
+# eval run_test
+# quit
+# EOF
 
 
 # #Actually run the pretty printer tests and make sure ERROR: is not printed:
