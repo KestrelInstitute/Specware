c69ae33e3151751ce76cfbd609f31a2b0549a989
Eric Smith <ewsmith@kestrel.edu>
2014-07-30 07:04:38 +0000

    Vastly simplified bootstrap script, for discussion, not used yet.
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@9390 7b97033b-253e-4a5c-9b07-e25f9089a9fd

---
+++ bin/specware-batch.sh
@@ -0,0 +1,29 @@
+# This script reads commands from stdin and sends them to Specware, to
+# be executed in batch mode.  It can be used with a 'here document'.
+
+# Example of use:
+
+# specware-batch.sh <<EOF
+# proc ${SPECWARE4}/Library/CGen/Deep/C
+# proc ${SPECWARE4}/Library/CGen/Deep/CPrettyPrinter_Tests
+# ctext ${SPECWARE4}/Library/CGen/Deep/CPrettyPrinter_Tests
+# e 1+1
+# e run_test
+# e 2+3
+# quit
+# EOF
+
+
+# If SWPATH isn't set, things currently fail in a confusing way:
+# To override this, set SWPATH before calling this script:
+if [ -z $SWPATH ] ; then
+    SWPATH="$SPECWARE4:/"
+fi
+export SWPATH
+
+#FIXME: Perhaps die if this is not set by the caller?
+# To override SBCL_SIZE, preset SBCL_SIZE before invoking this script.
+SBCL_SIZE="${SBCL_SIZE:=2000}"
+
+
+"$SPECWARE4"/bin/specware.exe --dynamic-space-size $SBCL_SIZE --eval "(progn (setq Emacs::*use-emacs-interface?* nil) (Specware::initializeSpecware-0) (SWShell::process-batch-commands) (sb-unix:unix-exit 0))"
