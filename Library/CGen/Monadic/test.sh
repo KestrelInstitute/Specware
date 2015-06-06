0c55fe973b7a432b2e62b23c942bf13f5d0aae7b
Eric Smith <ewsmith@kestrel.edu>
2015-06-06 01:18:06 +0000

    Add test script.
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@9889 7b97033b-253e-4a5c-9b07-e25f9089a9fd

---
+++ Library/CGen/Monadic/test.sh
@@ -0,0 +1,19 @@
+#Check the number of arguments supplied:
+if [ $# -ne 1 ]
+then
+    echo "test.sh: Error: Argument must be the Specware installation to use."
+    exit 1
+fi
+
+SPECWARE4=$1
+echo "Testing Library/CGen/Monadic:"
+
+run-proc.sh ${SPECWARE4} ${SPECWARE4}/Library/CGen/Monadic/C_DSL.sw
+run-proc.sh ${SPECWARE4} ${SPECWARE4}/Library/CGen/Monadic/C_Permissions.sw
+run-proc.sh ${SPECWARE4} ${SPECWARE4}/Library/CGen/Monadic/C_Predicates.sw
+run-proc.sh ${SPECWARE4} ${SPECWARE4}/Library/CGen/Monadic/CPrettyPrinter.sw
+run-proc.sh ${SPECWARE4} ${SPECWARE4}/Library/CGen/Monadic/C.sw
+run-proc.sh ${SPECWARE4} ${SPECWARE4}/Library/CGen/Monadic/GenerateC.sw
+run-proc.sh ${SPECWARE4} ${SPECWARE4}/Library/CGen/Monadic/TraceMonad_Simple.sw
+run-proc.sh ${SPECWARE4} ${SPECWARE4}/Library/CGen/Monadic/TraceMonad.sw
+
