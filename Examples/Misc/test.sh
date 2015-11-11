240967109afeca6c3844968b923245e806e66cc9
Eric Smith <ewsmith@kestrel.edu>
2015-11-11 01:04:32 +0000

    Update test script for Isabelle 2015.
    git-svn-id: svn+ssh://svn.kestrel.edu/Specware/trunk@10073 7b97033b-253e-4a5c-9b07-e25f9089a9fd

--- Examples/Misc/test.sh
+++ Examples/Misc/test.sh
@@ -1,12 +1,12 @@
 #Check the number of arguments supplied:
 if [ $# -ne 2 ]
 then
-    echo "test.sh: Error: Argument must be the Specware installation to test and ISABELLE_2014_ROOT."
+    echo "test.sh: Error: Argument must be the Specware installation to test and ISABELLE_2015_ROOT."
     exit 1
 fi
 
 SPECWARE4=$1
-ISABELLE_2014_ROOT=$2
+ISABELLE_2015_ROOT=$2
 
 
 echo "Running a tiny test of the Transform shell."
@@ -29,5 +29,5 @@ run-gen-obligs.sh ${SPECWARE4} ${SPECWARE4}/Examples/Misc/TwoMorphisms#B
 run-gen-obligs.sh ${SPECWARE4} ${SPECWARE4}/Examples/Misc/TwoMorphisms#CheckRqmtsB
 run-gen-obligs.sh ${SPECWARE4} ${SPECWARE4}/Examples/Misc/TwoMorphisms#C
 run-gen-obligs.sh ${SPECWARE4} ${SPECWARE4}/Examples/Misc/TwoMorphisms#CheckRqmtsC
-run-isabelle2014-build.sh ${SPECWARE4}/Examples/Misc/Isa ${ISABELLE_2014_ROOT}
+run-isabelle2015-build.sh ${SPECWARE4}/Examples/Misc/Isa ${ISABELLE_2015_ROOT}
 
